49027cf7341525b21b6c8344903bd7adbe01297c
Zealy-Rockx <jigao@rockx.com>
2022-05-17 09:50:28 +0800

    Add function to deploy AvaLido contract, plus improvement on AvaLido mock testing.

---
+++ mocks/avalido_staker/deploy.go
@@ -0,0 +1,31 @@
+package avalido_staker
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pkg/errors"
+	"math/big"
+	"time"
+)
+
+func Deploy(log logger.Logger, chainId *big.Int, client *ethclient.Client, privKey *ecdsa.PrivateKey, mpcCoordinatorAddr *common.Address) (*common.Address, *AvaLido, error) {
+	signer, err := bind.NewKeyedTransactorWithChainID(privKey, chainId)
+	log.FatalOnError(err, "Failed to create transaction signer", logger.Field{"error", err})
+
+	addr, tx, avalido, err := DeployAvaLido(signer, client, *mpcCoordinatorAddr)
+	log.FatalOnError(err, "Failed to deploy AvaLido smart contract", logger.Field{"error", err})
+
+	time.Sleep(time.Second * 5)
+	rcp, err := client.TransactionReceipt(context.Background(), tx.Hash())
+	if rcp.Status != 1 {
+		log.Fatal("Transaction failed", logger.Field{"receipt", spew.Sdump(rcp)})
+		return nil, nil, errors.Errorf("transaction failed, receipt: %s", spew.Sdump(rcp))
+	}
+
+	return &addr, avalido, nil
+}
