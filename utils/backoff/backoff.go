b4a34640bb05434035dd8acc8812261bba8d4df6
Zealy-Rockx <jigao@rockx.com>
2022-05-24 11:32:42 +0800

    Add backoff retryFn and testings.

---
+++ utils/backoff/backoff.go
@@ -0,0 +1,42 @@
+package backoff
+
+import (
+	"context"
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/lestrrat-go/backoff/v2"
+	"github.com/pkg/errors"
+	"time"
+)
+
+func ExponentialForever() backoff.Policy {
+	p := backoff.Exponential(
+		backoff.WithMaxRetries(0),
+		backoff.WithMinInterval(time.Second),
+		backoff.WithMaxInterval(time.Minute),
+		backoff.WithMultiplier(1.5),
+		backoff.WithJitterFactor(0.05),
+	)
+
+	return p
+}
+
+func RetryFn(log logger.Logger, ctx context.Context, policy backoff.Policy, fn func() error) error {
+	b := policy.Start(ctx)
+	var lastErr error
+	var lastRetryAt = time.Now()
+	var retryNum = 1
+	for backoff.Continue(b) {
+		err := fn()
+		if err == nil {
+			return nil
+		}
+		lastErr = err
+		log.Error("Retry",
+			[]logger.Field{{"error", err},
+				{"retryNum", retryNum},
+				{"retryAfter", time.Now().Sub(lastRetryAt).Seconds()}}...)
+		lastRetryAt = time.Now()
+		retryNum++
+	}
+	return errors.Wrapf(lastErr, "failed to get value")
+}
---
+++ utils/backoff/backoff_test.go
@@ -0,0 +1,79 @@
+package backoff
+
+import (
+	"context"
+	"fmt"
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/lestrrat-go/backoff/v2"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestExponentialForever(t *testing.T) {
+	p := ExponentialForever()
+
+	flakyFunc := func() error {
+		return errors.New("failed to dial ...")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	retryFunc := func() error {
+		b := p.Start(ctx)
+		var lastErr error
+		var lastRetryAt = time.Now()
+		var retryNum = 1
+		for backoff.Continue(b) {
+			err := flakyFunc()
+			if err == nil {
+				return nil
+			}
+			lastErr = err
+			fmt.Printf("Failed to get value, error: %v, retried after: %v seconds, %v times \n",
+				err, time.Now().Sub(lastRetryAt).Seconds(), retryNum)
+			lastRetryAt = time.Now()
+			retryNum++
+		}
+		return errors.Wrapf(lastErr, "failed to get value")
+	}
+
+	err := retryFunc()
+	require.NotNil(t, err)
+}
+
+func TestRetryFn(t *testing.T) {
+	p := ExponentialForever()
+
+	type scenario struct {
+		name    string
+		fn      func() error
+		wantErr bool
+	}
+
+	flakyFuncErr := func() error {
+		return errors.New("failed to dial ...")
+	}
+
+	flakyFuncNil := func() error {
+		return nil
+	}
+
+	scenarios := []scenario{
+		{"Fn returns error", flakyFuncErr, true},
+		{"Fn returns nil", flakyFuncNil, false},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			err := RetryFn(logger.Default(), ctx, p, scenario.fn)
+			assert.True(t, err != nil == scenario.wantErr)
+		})
+	}
+}
