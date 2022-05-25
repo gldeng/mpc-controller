9faab85c84342c8d3531e318a150154aecf653e9
Zealy-Rockx <jigao@rockx.com>
2022-05-25 16:03:26 +0800

    Delete coordinator wrapper.

--- contract/coordinator.go
+++ contract/coordinator.go
@@ -1,81 +1,71 @@
 package contract
 
-import (
-	"crypto/ecdsa"
-	"github.com/avalido/mpc-controller/logger"
-	"github.com/brianvoe/gofakeit/v6"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/pkg/errors"
-	"math/big"
-)
-
 // todo: consider remove this extra wrapper
-
-type Coordinator struct {
-	*MpcCoordinator
-	chainId *big.Int
-	log     logger.Logger
-}
-
-// NewCoordinator creates a new singleton instance of MpcCoordinator, bound to a specific deployed coordinator.
-func NewCoordinator(log logger.Logger, chainID *big.Int, contractAddress *common.Address, contractBackend bind.ContractBackend) (*Coordinator, error) {
-	c, err := NewMpcCoordinator(*contractAddress, contractBackend)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to create new coordinator instance")
-	}
-
-	return &Coordinator{c, chainID, log}, nil
-}
-
-// todo: check receipt to see whether a group created successfully or not
-
-func (c *Coordinator) CreateGroup_(txSenderPrivKey *ecdsa.PrivateKey, participantPubKeys [][]byte, threshold int64) (groupID []byte, err error) {
-	signer, err := bind.NewKeyedTransactorWithChainID(txSenderPrivKey, c.chainId)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to create a transaction signer with private key %q.", txSenderPrivKey)
-	}
-
-	txn, err := c.CreateGroup(signer, participantPubKeys[:], big.NewInt(threshold))
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to create group.")
-	}
-
-	c.log.Debug("Sent a transaction to create group.",
-		logger.Field{"participants", len(participantPubKeys)},
-		logger.Field{"threshold", threshold},
-		logger.Field{"txHashHex", txn.Hash().Hex()})
-
-	return []byte(gofakeit.UUID()), nil // todo: return real created group id, maybe a ethclient is required, see deploy.go as a exmaple.
-}
-
-func (c *Coordinator) RequestKeygen_(txSenderPrivKey *ecdsa.PrivateKey, groupId []byte) error {
-	signer, err := bind.NewKeyedTransactorWithChainID(txSenderPrivKey, c.chainId)
-	if err != nil {
-		return errors.Wrapf(err, "failed to create a transaction signer with private key %q.", txSenderPrivKey)
-	}
-
-	var groupId32 [32]byte
-	copy(groupId32[:], groupId)
-
-	txn, err := c.RequestKeygen(signer, groupId32)
-	if err != nil {
-		return errors.Wrapf(err, "failed to request keygen.")
-	}
-	c.log.Debug("Sent a transaction for keygen.", logger.Field{"txHashHex", txn.Hash().Hex()})
-	return nil
-}
-
-func (c *Coordinator) RequestStake_(txSenderPrivKey *ecdsa.PrivateKey, publicKey []byte, nodeID string, amount *big.Int, startTime *big.Int, endTime *big.Int) error {
-	signer, err := bind.NewKeyedTransactorWithChainID(txSenderPrivKey, c.chainId)
-	if err != nil {
-		return errors.Wrapf(err, "failed to create a transaction signer with private key %q.", txSenderPrivKey)
-	}
-
-	txn, err := c.RequestStake(signer, publicKey, nodeID, amount, startTime, endTime)
-	if err != nil {
-		return errors.Wrapf(err, "failed to request stake.")
-	}
-	c.log.Debug("Sent a transaction for stake.", logger.Field{"txHashHex", txn.Hash().Hex()})
-	return nil
-}
+//
+//type Coordinator struct {
+//	*MpcCoordinator
+//	chainId *big.Int
+//	log     logger.Logger
+//}
+//
+//// NewCoordinator creates a new singleton instance of MpcCoordinator, bound to a specific deployed coordinator.
+//func NewCoordinator(log logger.Logger, chainID *big.Int, contractAddress *common.Address, contractBackend bind.ContractBackend) (*Coordinator, error) {
+//	c, err := NewMpcCoordinator(*contractAddress, contractBackend)
+//	if err != nil {
+//		return nil, errors.Wrap(err, "failed to create new coordinator instance")
+//	}
+//
+//	return &Coordinator{c, chainID, log}, nil
+//}
+//
+//// todo: check receipt to see whether a group created successfully or not
+//
+//func (c *Coordinator) CreateGroup_(txSenderPrivKey *ecdsa.PrivateKey, participantPubKeys [][]byte, threshold int64) (groupID []byte, err error) {
+//	signer, err := bind.NewKeyedTransactorWithChainID(txSenderPrivKey, c.chainId)
+//	if err != nil {
+//		return nil, errors.Wrapf(err, "failed to create a transaction signer with private key %q.", txSenderPrivKey)
+//	}
+//
+//	txn, err := c.CreateGroup(signer, participantPubKeys[:], big.NewInt(threshold))
+//	if err != nil {
+//		return nil, errors.Wrapf(err, "failed to create group.")
+//	}
+//
+//	c.log.Debug("Sent a transaction to create group.",
+//		logger.Field{"participants", len(participantPubKeys)},
+//		logger.Field{"threshold", threshold},
+//		logger.Field{"txHashHex", txn.Hash().Hex()})
+//
+//	return []byte(gofakeit.UUID()), nil // todo: return real created group id, maybe a ethclient is required, see deploy.go as a exmaple.
+//}
+//
+//func (c *Coordinator) RequestKeygen_(txSenderPrivKey *ecdsa.PrivateKey, groupId []byte) error {
+//	signer, err := bind.NewKeyedTransactorWithChainID(txSenderPrivKey, c.chainId)
+//	if err != nil {
+//		return errors.Wrapf(err, "failed to create a transaction signer with private key %q.", txSenderPrivKey)
+//	}
+//
+//	var groupId32 [32]byte
+//	copy(groupId32[:], groupId)
+//
+//	txn, err := c.RequestKeygen(signer, groupId32)
+//	if err != nil {
+//		return errors.Wrapf(err, "failed to request keygen.")
+//	}
+//	c.log.Debug("Sent a transaction for keygen.", logger.Field{"txHashHex", txn.Hash().Hex()})
+//	return nil
+//}
+//
+//func (c *Coordinator) RequestStake_(txSenderPrivKey *ecdsa.PrivateKey, publicKey []byte, nodeID string, amount *big.Int, startTime *big.Int, endTime *big.Int) error {
+//	signer, err := bind.NewKeyedTransactorWithChainID(txSenderPrivKey, c.chainId)
+//	if err != nil {
+//		return errors.Wrapf(err, "failed to create a transaction signer with private key %q.", txSenderPrivKey)
+//	}
+//
+//	txn, err := c.RequestStake(signer, publicKey, nodeID, amount, startTime, endTime)
+//	if err != nil {
+//		return errors.Wrapf(err, "failed to request stake.")
+//	}
+//	c.log.Debug("Sent a transaction for stake.", logger.Field{"txHashHex", txn.Hash().Hex()})
+//	return nil
+//}
--- contract/deploy.go
+++ contract/deploy.go
@@ -1,68 +1,56 @@
 package contract
 
-import (
-	"context"
-	"crypto/ecdsa"
-	"github.com/avalido/mpc-controller/logger"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/pkg/errors"
-	"math/big"
-	"time"
-)
-
-var Gas int64 = 8000000
-var BaseFee int64 = 300_000_000_000
-
-// todo: consider remove this function, which has been moved into utils/contract package.
-
-func Deploy(chainId *big.Int, client *ethclient.Client, privKey *ecdsa.PrivateKey, bytecodeJSON string) (*common.Address, *types.Receipt, error) {
-	account := crypto.PubkeyToAddress(privKey.PublicKey)
-	nonce, err := client.NonceAt(context.Background(), account, nil)
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to query nonce for account %q.", account)
-	}
-
-	bytecodeBytes := common.Hex2Bytes(bytecodeJSON)
-
-	txdata := &types.DynamicFeeTx{
-		ChainID:    chainId,
-		Nonce:      nonce,
-		To:         nil,
-		Gas:        uint64(Gas),
-		GasFeeCap:  big.NewInt(BaseFee), // maxgascost = 2.1ether
-		GasTipCap:  big.NewInt(1),
-		AccessList: nil,
-		Data:       bytecodeBytes,
-	}
-	tx := types.NewTx(txdata)
-	signer := types.LatestSignerForChainID(chainId)
-	signature, err := crypto.Sign(signer.Hash(tx).Bytes(), privKey)
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to sign transaction %v with private key %v.", tx, privKey)
-	}
-
-	txSigned, err := tx.WithSignature(signer, signature)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to generate new signed transaction")
-	}
-
-	err = client.SendTransaction(context.Background(), txSigned)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to send transaction")
-	}
-	txHash := txSigned.Hash()
-
-	time.Sleep(5 * time.Second)
-	rcp, err := client.TransactionReceipt(context.Background(), txHash)
-	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to query transaction receipt for txHash %q.", txHash.Hex())
-	}
-	logger.Debug("Deployed a smart contract",
-		logger.Field{"txHash", txHash.Hex()},
-		logger.Field{"receipt", rcp})
-
-	return &rcp.ContractAddress, rcp, nil
-}
+//
+//var Gas int64 = 8000000
+//var BaseFee int64 = 300_000_000_000
+//
+//// todo: consider remove this function, which has been moved into utils/contract package.
+//
+//func Deploy(chainId *big.Int, client *ethclient.Client, privKey *ecdsa.PrivateKey, bytecodeJSON string) (*common.Address, *types.Receipt, error) {
+//	account := crypto.PubkeyToAddress(privKey.PublicKey)
+//	nonce, err := client.NonceAt(context.Background(), account, nil)
+//	if err != nil {
+//		return nil, nil, errors.Wrapf(err, "failed to query nonce for account %q.", account)
+//	}
+//
+//	bytecodeBytes := common.Hex2Bytes(bytecodeJSON)
+//
+//	txdata := &types.DynamicFeeTx{
+//		ChainID:    chainId,
+//		Nonce:      nonce,
+//		To:         nil,
+//		Gas:        uint64(Gas),
+//		GasFeeCap:  big.NewInt(BaseFee), // maxgascost = 2.1ether
+//		GasTipCap:  big.NewInt(1),
+//		AccessList: nil,
+//		Data:       bytecodeBytes,
+//	}
+//	tx := types.NewTx(txdata)
+//	signer := types.LatestSignerForChainID(chainId)
+//	signature, err := crypto.Sign(signer.Hash(tx).Bytes(), privKey)
+//	if err != nil {
+//		return nil, nil, errors.Wrapf(err, "failed to sign transaction %v with private key %v.", tx, privKey)
+//	}
+//
+//	txSigned, err := tx.WithSignature(signer, signature)
+//	if err != nil {
+//		return nil, nil, errors.Wrap(err, "failed to generate new signed transaction")
+//	}
+//
+//	err = client.SendTransaction(context.Background(), txSigned)
+//	if err != nil {
+//		return nil, nil, errors.Wrap(err, "failed to send transaction")
+//	}
+//	txHash := txSigned.Hash()
+//
+//	time.Sleep(5 * time.Second)
+//	rcp, err := client.TransactionReceipt(context.Background(), txHash)
+//	if err != nil {
+//		return nil, nil, errors.Wrapf(err, "failed to query transaction receipt for txHash %q.", txHash.Hex())
+//	}
+//	logger.Debug("Deployed a smart contract",
+//		logger.Field{"txHash", txHash.Hex()},
+//		logger.Field{"receipt", rcp})
+//
+//	return &rcp.ContractAddress, rcp, nil
+//}
