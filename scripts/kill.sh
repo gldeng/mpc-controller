68d0d817a36ed6d23db2b9e529dc4505787201ec
Zealy-Rockx <jigao@rockx.com>
2022-05-29 14:26:19 +0800

    Improve kill.sh.

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-PROCESS_AVALANCHE=$(ps -aux | grep avalanchego | xargs | wc -l)
+PROCESS_AVALANCHE=$(ps -aux | grep avalanchego | wc -l)
 if [ $PROCESS_AVALANCHE -gt 1 ]; then
   echo "Killing avalanchego"
   pkill -f avalanchego
 fi
 
-PROCESS_MPC_CONTROLLER=$(ps -aux | grep mpc-controller | xargs | wc -l)
-if [ $PROCESS_MPC_CONTROLLER -gt 1 ]; then
+PROCESS_MPC_CONTROLLER=$(ps -aux | grep mpc-controller | wc -l)
+if [ $PROCESS_MPC_CONTROLLER -gt 3 ]; then
   echo "Killing mpc-controller"
   pkill -f mpc-controller
 fi
 
-PROCESS_MPC_SERVER=$(ps -aux | grep mpc-server | xargs | wc -l)
+PROCESS_MPC_SERVER=$(ps -aux | grep mpc-server | wc -l)
 if [ $PROCESS_MPC_SERVER -gt 1 ]; then
   echo "Killing mpc-server"
   pkill -f mpc-server
 fi
-
-
