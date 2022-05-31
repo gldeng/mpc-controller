0f5c47de0da950e2e1ce52aad213d33708b7aaf7
Zealy-Rockx <jigao@rockx.com>
2022-05-31 14:27:50 +0800

    Improve clone.sh

--- tests/scripts/clone.sh
+++ tests/scripts/clone.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-cd ..
+mkdir -p /tmp/mpctest
 
-if [ ! -d "avalanchego" ]; then
-  git clone git@github.com:ava-labs/avalanchego.git
+mkdir -p /tmp/mpctest/mpc-controller
+
+if [ ! -d "/tmp/mpctest/avalanchego" ]; then
+  git clone git@github.com:ava-labs/avalanchego.git /tmp/mpctest/avalanchego
 fi
 
-if [ ! -d "mpc-server" ]; then
-  git clone git@github.com:AvaLido/mpc-server.git
+if [ ! -d "/tmp/mpctest/mpc-server" ]; then
+  git clone git@github.com:AvaLido/mpc-server.git /tmp/mpctest/mpc-server
 fi
 
-cd mpc-controller
+if [ ! -d "/tmp/mpctest/contracts" ]; then
+  LAST_WD=$(pwd)
+  cd /tmp/mpctest/
+
+  forge init contracts
+  cp -a $LAST_WD/contract/src/. contracts/src/
+  cp -a $LAST_WD/contract/lib/. contracts/lib/
+
+  rm contracts/src/Contract.sol contracts/test/Contract.t.sol
+  cd $LAST_WD
+fi
