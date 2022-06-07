8a2afd638af20271815ed9ae236f882de610d4ba
Zealy-Rockx <jigao@rockx.com>
2022-06-08 05:57:08 +0800

    Implement WatchKeygenRequestAdded.

--- contract/wrappers/filter_wrapper.go
+++ contract/wrappers/filter_wrapper.go
@@ -45,3 +45,34 @@ func (m *MpcManagerFilterWrapper) WatchParticipantAdded(ctx context.Context, pub
 
 	return sink, nil
 }
+
+func (m *MpcManagerFilterWrapper) WatchKeygenRequestAdded(ctx context.Context, groupId [][32]byte) (<-chan *contract.MpcManagerKeygenRequestAdded, error) {
+	sink := make(chan *contract.MpcManagerKeygenRequestAdded)
+
+	err := backoff.RetryFnExponentialForever(m.Logger, ctx, func() error {
+		sub, err := m.MpcManagerFilterer.WatchKeygenRequestAdded(nil, sink, groupId)
+		if err != nil {
+			m.Error("Failed to watch KeygenRequestAdded event", logger.Field{"error", err})
+			return errors.WithStack(err)
+		}
+
+		go func() {
+			for {
+				select {
+				case <-ctx.Done():
+					sub.Unsubscribe()
+				case err := <-sub.Err():
+					m.ErrorOnError(err, "Got an error during watching KeygenRequestAdded event", logger.Field{"error", err})
+				}
+			}
+		}()
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return sink, nil
+}
