670cf35ef0d544d895189a2ce62a3593159c3532
Zealy-Rockx <jigao@rockx.com>
2022-06-08 06:20:05 +0800

    Add extra store method.

--- storage/storer.go
+++ storage/storer.go
@@ -1,6 +1,9 @@
 package storage
 
-import "context"
+import (
+	"context"
+	"math/big"
+)
 
 type Storer interface {
 	StoreGroupInfo(ctx context.Context, g *GroupInfo) error
@@ -11,9 +14,15 @@ type Storer interface {
 	LoadParticipantInfo(ctx context.Context, pubKeyHashHex, groupId string) (*ParticipantInfo, error)
 	LoadParticipantInfos(ctx context.Context, pubKeyHashHex string) ([]*ParticipantInfo, error)
 
+	GetIndex(ctx context.Context, partiPubKeyHashHex, genPubKeyHexHex string) (*big.Int, error)
+	GetGroupIds(ctx context.Context, partiPubKeyHashHex string) ([][32]byte, error)
+	GetPubKeys(ctx context.Context, partiPubKeyHashHex string) ([][]byte, error)
+
 	StoreGeneratedPubKeyInfo(ctx context.Context, genPubKeyInfo *GeneratedPubKeyInfo) error
 	LoadGeneratedPubKeyInfo(ctx context.Context, pubKeyHashHex string) (*GeneratedPubKeyInfo, error)
 	LoadGeneratedPubKeyInfos(ctx context.Context, groupIdHexs []string) ([]*GeneratedPubKeyInfo, error)
+
+	GetPariticipantKeys(ctx context.Context, genPubKeyHashHex string, indices []*big.Int) ([]string, error)
 
 	StoreKeygenRequestInfo(ctx context.Context, keygenReqInfo *KeygenRequestInfo) error
 	LoadKeygenRequestInfo(ctx context.Context, reqIdHex string) (*KeygenRequestInfo, error)
