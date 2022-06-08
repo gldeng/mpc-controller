063539fc2d5a4bf310256af8beb7bc6044154cdd
Zealy-Rockx <jigao@rockx.com>
2022-06-08 10:15:46 +0800

    Add RpcEthClientWrapper.

--- chain/rpc_ethclient.go
+++ chain/rpc_ethclient.go
@@ -1,1 +1,56 @@
 package chain
+
+import (
+	"context"
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/avalido/mpc-controller/utils/backoff"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/pkg/errors"
+	"math/big"
+)
+
+type RpcEthClientWrapper struct {
+	logger.Logger
+	*ethclient.Client
+}
+
+func (m *RpcEthClientWrapper) TransactionReceipt(ctx context.Context, txHash common.Hash) (r *types.Receipt, err error) {
+	err = backoff.RetryFnExponentialForever(m.Logger, ctx, func() error {
+		r, err = m.Client.TransactionReceipt(ctx, txHash)
+		if err != nil {
+			m.Error("Failed to query transaction receipt", logger.Field{"error", err})
+			return errors.WithStack(err)
+		}
+
+		return nil
+	})
+	return
+}
+
+func (m *RpcEthClientWrapper) NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (nonce uint64, err error) {
+	err = backoff.RetryFnExponentialForever(m.Logger, ctx, func() error {
+		nonce, err = m.Client.NonceAt(ctx, account, blockNumber)
+		if err != nil {
+			m.Error("Failed to query nonce", logger.Field{"error", err})
+			return errors.WithStack(err)
+		}
+
+		return nil
+	})
+	return
+}
+
+func (m *RpcEthClientWrapper) BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (bl *big.Int, err error) {
+	err = backoff.RetryFnExponentialForever(m.Logger, ctx, func() error {
+		bl, err = m.Client.BalanceAt(ctx, account, blockNumber)
+		if err != nil {
+			m.Error("Failed to query balance", logger.Field{"error", err})
+			return errors.WithStack(err)
+		}
+
+		return nil
+	})
+	return
+}
