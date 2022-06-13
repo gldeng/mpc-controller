d679948a9df049b02361b90c3fc43d016f188a4c
Zealy-Rockx <jigao@rockx.com>
2022-06-13 17:58:34 +0800

    Add interfaces regarding MpcManager.

---
+++ contract/interfaces.go
@@ -0,0 +1,49 @@
+package contract
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
+	"math/big"
+)
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Interfaces regarding MpcManager
+
+// Callers
+
+type CallerGetGroup interface {
+	GetGroup(ctx context.Context, groupId [32]byte) (Participants [][]byte, Threshold *big.Int, err error)
+}
+
+// Transactor
+
+type TransactorJoinRequest interface {
+	JoinRequest(ctx context.Context, opts *bind.TransactOpts, requestId *big.Int, myIndex *big.Int) (*types.Transaction, error)
+}
+
+type TransactorReportGeneratedKey interface {
+	ReportGeneratedKey(ctx context.Context, opts *bind.TransactOpts, groupId [32]byte, myIndex *big.Int, generatedPublicKey []byte) (*types.Transaction, error)
+}
+
+// Filters
+
+type FilterParticipantAdded interface {
+	WatchParticipantAdded(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerParticipantAdded, error)
+}
+
+type FilterKeygenRequestAdded interface {
+	WatchKeygenRequestAdded(ctx context.Context, groupId [][32]byte) (<-chan *MpcManagerKeygenRequestAdded, error)
+}
+
+type FilterKeyGenerated interface {
+	WatchKeyGenerated(ctx context.Context, groupId [][32]byte) (<-chan *MpcManagerKeyGenerated, error)
+}
+
+type FilterStakeRequestAdded interface {
+	WatchStakeRequestAdded(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerStakeRequestAdded, error)
+}
+
+type FilterStakeRequestStarted interface {
+	WatchStakeRequestStarted(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerStakeRequestStarted, error)
+}
