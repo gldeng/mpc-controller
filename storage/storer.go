4b52dc2781affbdc32eb6713624575a90a429a78
Zealy-Rockx <jigao@rockx.com>
2022-06-13 17:19:03 +0800

    Divide storer interfaces.

--- storage/storer.go
+++ storage/storer.go
@@ -5,27 +5,98 @@ import (
 	"math/big"
 )
 
+// ---------------------------------------------------------------------------------------------------------------------
+// Interfaces regarding storage
+
 type Storer interface {
-	StoreGroupInfo(ctx context.Context, g *GroupInfo) error
-	LoadGroupInfo(ctx context.Context, groupIdHex string) (*GroupInfo, error)
-	LoadGroupInfos(ctx context.Context) ([]*GroupInfo, error)
+	StorerStoreGroupInfo
+	StorerLoadGroupInfo
+	StorerLoadGroupInfos
 
-	StoreParticipantInfo(ctx context.Context, p *ParticipantInfo) error
-	LoadParticipantInfo(ctx context.Context, pubKeyHashHex, groupId string) (*ParticipantInfo, error)
-	LoadParticipantInfos(ctx context.Context, pubKeyHashHex string) ([]*ParticipantInfo, error)
+	StorerStoreParticipantInfo
+	StorerLoadParticipantInfo
+	StorerLoadParticipantInfos
 
-	GetIndex(ctx context.Context, partiPubKeyHashHex, genPubKeyHexHex string) (*big.Int, error)
-	GetGroupIds(ctx context.Context, partiPubKeyHashHex string) ([][32]byte, error)
-	GetPubKeys(ctx context.Context, partiPubKeyHashHex string) ([][]byte, error)
+	StorerGetParticipantIndex
+	StorerGetGroupIds
+	StorerGetPubKeys
 
-	StoreGeneratedPubKeyInfo(ctx context.Context, genPubKeyInfo *GeneratedPubKeyInfo) error
-	LoadGeneratedPubKeyInfo(ctx context.Context, pubKeyHashHex string) (*GeneratedPubKeyInfo, error)
-	LoadGeneratedPubKeyInfos(ctx context.Context, groupIdHexs []string) ([]*GeneratedPubKeyInfo, error)
+	StorerStoreGeneratedPubKeyInfo
+	StorerLoadGeneratedPubKeyInfo
+	StorerLoadGeneratedPubKeyInfos
 
-	GetPariticipantKeys(ctx context.Context, genPubKeyHashHex string, indices []*big.Int) ([]string, error)
+	StorerGetPariticipantKeys
 
-	StoreKeygenRequestInfo(ctx context.Context, keygenReqInfo *KeygenRequestInfo) error
-	LoadKeygenRequestInfo(ctx context.Context, reqIdHex string) (*KeygenRequestInfo, error)
+	StorerStoreKeygenRequestInfo
+	StorerLoadKeygenRequestInfo
 
 	Close() error
+}
+
+// Group
+
+type StorerStoreGroupInfo interface {
+	StoreGroupInfo(ctx context.Context, g *GroupInfo) error
+}
+
+type StorerLoadGroupInfo interface {
+	LoadGroupInfo(ctx context.Context, groupIdHex string) (*GroupInfo, error)
+}
+
+type StorerLoadGroupInfos interface {
+	LoadGroupInfos(ctx context.Context) ([]*GroupInfo, error)
+}
+
+// Participant
+
+type StorerStoreParticipantInfo interface {
+	StoreParticipantInfo(ctx context.Context, p *ParticipantInfo) error
+}
+
+type StorerLoadParticipantInfo interface {
+	LoadParticipantInfo(ctx context.Context, pubKeyHashHex, groupId string) (*ParticipantInfo, error)
+}
+
+type StorerLoadParticipantInfos interface {
+	LoadParticipantInfos(ctx context.Context, pubKeyHashHex string) ([]*ParticipantInfo, error)
+}
+
+type StorerGetParticipantIndex interface {
+	GetIndex(ctx context.Context, partiPubKeyHashHex, genPubKeyHexHex string) (*big.Int, error)
+}
+
+type StorerGetGroupIds interface {
+	GetGroupIds(ctx context.Context, partiPubKeyHashHex string) ([][32]byte, error)
+}
+
+type StorerGetPubKeys interface {
+	GetPubKeys(ctx context.Context, partiPubKeyHashHex string) ([][]byte, error)
+}
+
+// Generated public key
+
+type StorerStoreGeneratedPubKeyInfo interface {
+	StoreGeneratedPubKeyInfo(ctx context.Context, genPubKeyInfo *GeneratedPubKeyInfo) error
+}
+
+type StorerLoadGeneratedPubKeyInfo interface {
+	LoadGeneratedPubKeyInfo(ctx context.Context, pubKeyHashHex string) (*GeneratedPubKeyInfo, error)
+}
+
+type StorerLoadGeneratedPubKeyInfos interface {
+	LoadGeneratedPubKeyInfos(ctx context.Context, groupIdHexs []string) ([]*GeneratedPubKeyInfo, error)
+}
+
+type StorerGetPariticipantKeys interface {
+	GetPariticipantKeys(ctx context.Context, genPubKeyHashHex string, indices []*big.Int) ([]string, error)
+}
+
+// Keygen request info
+
+type StorerStoreKeygenRequestInfo interface {
+	StoreKeygenRequestInfo(ctx context.Context, keygenReqInfo *KeygenRequestInfo) error
+}
+
+type StorerLoadKeygenRequestInfo interface {
+	LoadKeygenRequestInfo(ctx context.Context, reqIdHex string) (*KeygenRequestInfo, error)
 }
