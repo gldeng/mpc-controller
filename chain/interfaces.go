07cb5775fba68cc1fa609b6ec960e33f8ec1afff
Zealy-Rockx <jigao@rockx.com>
2022-06-14 09:38:31 +0800

    Add interfaces regarding chain status.

---
+++ chain/interfaces.go
@@ -0,0 +1,23 @@
+package chain
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"math/big"
+)
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Interfaces regarding chain status
+
+type TransactionReceipt interface {
+	TransactionReceipt(ctx context.Context, txHash common.Hash) (r *types.Receipt, err error)
+}
+
+type NonceAt interface {
+	NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (nonce uint64, err error)
+}
+
+type BalanceAt interface {
+	BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (bl *big.Int, err error)
+}
