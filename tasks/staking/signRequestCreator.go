cbbc2ddd86d902c743c3caf9cd1e54b61ace243c
Zealy-Rockx <jigao@rockx.com>
2022-06-15 10:37:57 +0800

    Add signRequestCreator.

---
+++ tasks/staking/signRequestCreator.go
@@ -0,0 +1,58 @@
+package staking
+
+import (
+	"github.com/avalido/mpc-controller/core"
+	"github.com/avalido/mpc-controller/utils/bytes"
+	"github.com/pkg/errors"
+	"strconv"
+)
+
+type signRequestCreator struct {
+	Task   *StakeTask
+	TaskID string
+	Nonce  uint64
+
+	NormalizedParticipantKeys []string
+	PubKeyHex                 string
+
+	reqNum    uint8
+	txHashHex string
+}
+
+// Todo: Consider applying State design pattern
+
+func (m *signRequestCreator) CreateSignRequest() (*core.SignRequest, error) {
+	switch m.reqNum {
+	case 0:
+		txHashBytes, err := m.Task.ExportTxHash()
+		if err != nil {
+			return nil, errors.Wrapf(err, "Failed to create ExportTxHash")
+		}
+		m.txHashHex = bytes.BytesToHex(txHashBytes)
+
+		m.reqNum++
+	case 1:
+		txHashBytes, err := m.Task.ImportTxHash()
+		if err != nil {
+			return nil, errors.Wrapf(err, "Failed to create ImportTxHash")
+		}
+		m.txHashHex = bytes.BytesToHex(txHashBytes)
+
+		m.reqNum++
+	case 2:
+		txHashBytes, err := m.Task.AddDelegatorTxHash()
+		if err != nil {
+			return nil, errors.Wrapf(err, "Failed to create AddDelegatorTxHash")
+		}
+		m.txHashHex = bytes.BytesToHex(txHashBytes)
+	}
+
+	request := core.SignRequest{
+		RequestId:       m.TaskID + "-" + strconv.Itoa(int(m.reqNum)),
+		PublicKey:       m.PubKeyHex,
+		ParticipantKeys: m.NormalizedParticipantKeys,
+		Hash:            m.txHashHex,
+	}
+
+	return &request, nil
+}
