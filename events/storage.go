7362644c383f2fec104ea9d0d4c7d9a70a0799b1
Zealy-Rockx <jigao@rockx.com>
2022-06-16 18:47:21 +0800

    Add prefixes.

--- events/storage.go
+++ events/storage.go
@@ -20,11 +20,21 @@ type GeneratedPubKeyInfoStoredEvent struct {
 
 // Info types
 
+// Prefixes
+const (
+	prefixGroupInfo       = "groupInfo"
+	prefixParticipantInfo = "partyInfo"
+)
+
+// GroupInfo stored key format: prefixGroupInfo-GroupIdHex
+
 type GroupInfo struct {
 	GroupIdHex     string
 	PartPubKeyHexs []string
 	Threshold      uint64
 }
+
+// ParticipantInfo stored key format: prefixParticipantInfo-PubKeyHashHex-GroupIdHex
 
 type ParticipantInfo struct {
 	PubKeyHashHex string
