d79839f608648b8eb8605c7486f4b3362ade0a7d
Zealy-Rockx <jigao@rockx.com>
2022-06-16 16:28:27 +0800

    Redefine storage events.

--- events/storage.go
+++ events/storage.go
@@ -4,19 +4,36 @@ package events
 // Events concerning local storage
 
 type GroupInfoStoredEvent struct {
+	Key string
+	Val GroupInfo
+}
+
+type ParticipantInfoStoredEvent struct {
+	Key string
+	Val ParticipantInfoStored
+}
+
+type GeneratedPubKeyInfoStoredEvent struct {
+	Key string
+	Val GeneratedPubKeyInfo
+}
+
+// Info types
+
+type GroupInfo struct {
 	GroupIdHex     string
 	PartPubKeyHexs []string
 	Threshold      uint64
 }
 
-type ParticipantInfoStoredEvent struct {
+type ParticipantInfoStored struct {
 	PubKeyHashHex string
 	PubKeyHex     string
 	GroupIdHex    string
 	Index         uint64
 }
 
-type GeneratedPubKeyInfoStoredEvent struct {
+type GeneratedPubKeyInfo struct {
 	PubKeyHashHex string
 	PubKeyHex     string
 	GroupIdHex    string
