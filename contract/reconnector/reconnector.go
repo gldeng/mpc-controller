2ecadcf8e14714fab62e65143965eac7daf2655d
Zealy-Rockx <jigao@rockx.com>
2022-06-19 08:34:02 +0800

    Add ethWsClient reconnector.

--- contract/reconnector/reconnector.go
+++ contract/reconnector/reconnector.go
@@ -5,9 +5,7 @@ import (
 	"github.com/avalido/mpc-controller/dispatcher"
 	"github.com/avalido/mpc-controller/events"
 	"github.com/avalido/mpc-controller/logger"
-	"github.com/avalido/mpc-controller/utils/backoff"
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/pkg/errors"
+	"github.com/avalido/mpc-controller/utils/network"
 	"time"
 )
 
@@ -18,81 +16,33 @@ import (
 type ContractFilterReconnector struct {
 	logger.Logger
 
-	EthWsUrl    string
-	EthWsClient *ethclient.Client
+	Updater network.EthWsClientUpdater
 
 	Publisher dispatcher.Publisher
 }
 
 func (c *ContractFilterReconnector) Start(ctx context.Context) error {
 	go func() {
+		ticker := time.NewTicker(1 * time.Second)
 		for {
 			select {
 			case <-ctx.Done():
+				ticker.Stop()
 				return
-			default:
-				for {
-					if c.isDisconnected(ctx) {
-						select {
-						case <-ctx.Done():
-							return
-						default:
-							err := c.reConnect(ctx)
-							if err == nil {
-								c.Error("Failed to reconnect websocket", []logger.Field{
-									{"error", err},
-									{"url", c.EthWsUrl}}...)
-								break
-							}
-
-							select {
-							case <-ctx.Done():
-								return
-							default:
-								c.publishReconnectEvent(ctx)
-							}
-						}
+			case <-ticker.C:
+				client, isUpdated, err := c.Updater.NewEthWsClient(ctx)
+				if err != nil {
+					c.Error("Failed to check check connectivity of EthWsClient", []logger.Field{{"error", err}}...)
+					break
+				}
+				if isUpdated {
+					newEvt := events.ContractFiltererCreatedEvent{
+						Filterer: client,
 					}
+					c.Publisher.Publish(ctx, dispatcher.NewRootEventObject("ContractFilterReconnector", &newEvt, ctx))
 				}
 			}
 		}
-
 	}()
 	return nil
-}
-
-func (c *ContractFilterReconnector) isDisconnected(ctx context.Context) bool {
-	ticker := time.NewTicker(1 * time.Second)
-	for {
-		select {
-		case <-ctx.Done():
-			ticker.Stop()
-			return false
-		case <-ticker.C:
-			_, err := c.EthWsClient.NetworkID(ctx)
-			if err != nil {
-				return true
-			}
-		}
-	}
-}
-
-func (c *ContractFilterReconnector) reConnect(ctx context.Context) error {
-	err := backoff.RetryFn(c.Logger, ctx, backoff.ExponentialForever(), func() error {
-		client, err := ethclient.Dial(c.EthWsUrl)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-		c.EthWsClient = client
-		return nil
-	})
-	return errors.WithStack(err)
-}
-
-func (c *ContractFilterReconnector) publishReconnectEvent(ctx context.Context) {
-	newEvt := events.ContractFiltererCreatedEvent{
-		Filterer: c.EthWsClient,
-	}
-
-	c.Publisher.Publish(ctx, dispatcher.NewRootEventObject("ContractFilterReconnector", &newEvt, ctx))
 }
