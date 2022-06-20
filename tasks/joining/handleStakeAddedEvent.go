3f913dcf0bfd2b743419150f8d6e8c7278d457e3
Zealy-Rockx <jigao@rockx.com>
2022-06-20 10:04:47 +0800

    Remove getMyIndex method.

--- tasks/joining/handleStakeAddedEvent.go
+++ tasks/joining/handleStakeAddedEvent.go
@@ -2,12 +2,12 @@ package joining
 
 import (
 	"context"
+	"github.com/avalido/mpc-controller/cache"
 	"github.com/avalido/mpc-controller/chain"
 	"github.com/avalido/mpc-controller/contract"
 	"github.com/avalido/mpc-controller/dispatcher"
 	"github.com/avalido/mpc-controller/events"
 	"github.com/avalido/mpc-controller/logger"
-	"github.com/avalido/mpc-controller/storage"
 	"github.com/avalido/mpc-controller/utils/backoff"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -17,11 +17,6 @@ import (
 	"time"
 )
 
-type Storer interface {
-	storage.StorerLoadGeneratedPubKeyInfo
-	storage.StorerLoadParticipantInfo
-}
-
 // Accept event: *contract.MpcManagerStakeRequestAdded
 
 // Emit event: *events.JoinedRequestEvent
@@ -30,10 +25,9 @@ type StakeRequestAddedEventHandler struct {
 	Logger logger.Logger
 
 	MyPubKeyHashHex string
+	MyIndexGetter   cache.MyIndexGetter
 
 	Signer *bind.TransactOpts
-
-	Storer Storer
 
 	Receipter chain.Receipter
 
@@ -42,20 +36,17 @@ type StakeRequestAddedEventHandler struct {
 
 	Publisher dispatcher.Publisher
 
-	genPubKeyInfo *storage.GeneratedPubKeyInfo
+	participantInfoMap map[string]events.ParticipantInfo
 }
 
 func (eh *StakeRequestAddedEventHandler) Do(evtObj *dispatcher.EventObject) {
 	switch evt := evtObj.Event.(type) {
 	case *contract.MpcManagerStakeRequestAdded:
-		myIndex, err := eh.getMyIndex(evtObj.Context, evt.PublicKey.Hex())
-		if err != nil {
-			eh.Logger.Error("Failed to get my index", []logger.Field{
-				{"error", err},
-				{"pubKeyHashHex", evt.PublicKey.Hex()}}...)
+		genPubKeyHashHex := evt.PublicKey.Hex()
+		myIndex := eh.MyIndexGetter.GetMyIndex(eh.MyPubKeyHashHex, genPubKeyHashHex)
+		if myIndex == nil {
 			break
 		}
-
 		txHash, err := eh.joinRequest(evtObj.Context, myIndex, evt)
 		if err != nil || eh.checkReceipt(*txHash) != nil {
 			eh.Logger.Error("Failed to join request", []logger.Field{
@@ -124,17 +115,4 @@ func (eh *StakeRequestAddedEventHandler) checkReceipt(txHash common.Hash) error 
 	}
 
 	return nil
-}
-
-func (eh *StakeRequestAddedEventHandler) getMyIndex(ctx context.Context, genPubKeyHashHex string) (*big.Int, error) {
-	genPubKeyInfo, err := eh.Storer.LoadGeneratedPubKeyInfo(ctx, genPubKeyHashHex)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	eh.genPubKeyInfo = genPubKeyInfo
-	partInfo, err := eh.Storer.LoadParticipantInfo(ctx, eh.MyPubKeyHashHex, genPubKeyInfo.GroupIdHex)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-	return big.NewInt(int64(partInfo.Index)), nil
 }
