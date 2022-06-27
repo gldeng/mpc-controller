328346f6f9c3d7bd55d3b49a3680c1985919bfca
Zealy-Rockx <jigao@rockx.com>
2022-06-27 10:58:52 +0800

    Apply FromSigBytes.

--- utils/tx/pchain/signed_tx.go
+++ utils/tx/pchain/signed_tx.go
@@ -3,20 +3,15 @@ package pchain
 import (
 	"github.com/ava-labs/avalanchego/vms/components/verify"
 	"github.com/ava-labs/avalanchego/vms/platformvm"
-	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
+	fx "github.com/avalido/mpc-controller/utils/crypto/secp256k1fx"
 	"github.com/pkg/errors"
 )
 
 func SignedTx(unsignedTx platformvm.UnsignedTx, sig [65]byte) (*platformvm.Tx, error) {
-	cred := &secp256k1fx.Credential{
-		Sigs: make([][65]byte, 1),
-	}
-	copy(cred.Sigs[0][:], sig[:])
-
 	tx := &platformvm.Tx{
 		UnsignedTx: unsignedTx,
 		Creds: []verify.Verifiable{
-			cred,
+			fx.FromSigBytes(sig),
 		},
 	}
 
