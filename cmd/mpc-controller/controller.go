642f4f8a9dfa53125361ea7ba173d14e4de80611
Zealy-Rockx <jigao@rockx.com>
2022-06-29 16:58:01 +0800

    Rename files.

--- cmd/mpc-controller/controller.go
+++ cmd/mpc-controller/controller.go
@@ -15,7 +15,7 @@ import (
 	"github.com/avalido/mpc-controller/dispatcher"
 	"github.com/avalido/mpc-controller/support/keygen"
 	"github.com/avalido/mpc-controller/tasks/joining"
-	"github.com/avalido/mpc-controller/tasks/rewarding/tracker"
+	"github.com/avalido/mpc-controller/tasks/rewarding/stakingRewardUTXOFetcher"
 	"github.com/avalido/mpc-controller/tasks/staking"
 	"github.com/avalido/mpc-controller/utils/bytes"
 	myCrypto "github.com/avalido/mpc-controller/utils/crypto"
@@ -190,7 +190,7 @@ func NewController(ctx context.Context, c *cli.Context) *MpcController {
 		PChainIssueClient: pChainIssueCli,
 	}
 
-	rewardTrackerMaster := tracker.RewardTrackerMaster{
+	rewardTrackerMaster := stakingRewardUTXOFetcher.RewardTrackerMaster{
 		Logger:           myLogger,
 		Dispatcher:       myDispatcher,
 		RewardUTXOGetter: pChainIssueCli,
