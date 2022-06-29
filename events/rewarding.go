00c9565cf32e15e82b7cb384c0aee27839aba649
Zealy-Rockx <jigao@rockx.com>
2022-06-29 13:55:56 +0800

    Add StakingPeriodEndedEvent.

--- events/rewarding.go
+++ events/rewarding.go
@@ -1,12 +1,29 @@
 package events
 
-import "github.com/ava-labs/avalanchego/vms/components/avax"
+import (
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/vms/components/avax"
+)
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Events concerning rewarding
 
+type StakingPeriodEndedEvent struct {
+	AddDelegatorTxID ids.ID
+
+	RequestID   uint64
+	DelegateAmt uint64
+	StartTime   uint64
+	EndTime     uint64
+	NodeID      ids.NodeID
+
+	PubKeyHex     string
+	PChainAddress ids.ShortID
+}
+
 type RewardingTaskStartedEvent struct {
-	UTXO *avax.UTXO
+	PChainAddress ids.ShortID
+	UTXO          *avax.UTXO
 }
 
 type RewardingTaskDoneEvent struct {
