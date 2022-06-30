7ede385f5f7fc38e9510dc3856d98ed7a555385e
Zealy-Rockx <jigao@rockx.com>
2022-06-30 18:18:14 +0800

    Add StakingRewardUTXOReporterTestSuite.

---
+++ tasks/rewarding/stakingRewardUTXOReporter/stakingRewardUTXOReporter_test.go
@@ -0,0 +1,72 @@
+package stakingRewardUTXOReporter
+
+import (
+	"context"
+	"github.com/ava-labs/avalanchego/ids"
+	chainMocks "github.com/avalido/mpc-controller/chain/mocks"
+	contractMocks "github.com/avalido/mpc-controller/contract/mocks"
+	"github.com/avalido/mpc-controller/dispatcher"
+	dispatcherMocks "github.com/avalido/mpc-controller/dispatcher/mocks"
+	"github.com/avalido/mpc-controller/events"
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type StakingRewardUTXOReporterTestSuite struct {
+	suite.Suite
+}
+
+func (suite *StakingRewardUTXOReporterTestSuite) SetupTest() {}
+
+func (suite *StakingRewardUTXOReporterTestSuite) TestSignTx() {
+	require := suite.Require()
+
+	myTransactor := contractMocks.NewTransactorReportRewardUTXOs(suite.T())
+	myReceipter := chainMocks.NewReceipter(suite.T())
+	myDispatcher := dispatcherMocks.NewDispatcherrer(suite.T())
+
+	utxoFetchedEvt := &events.RewardUTXOsFetchedEvent{ // todo: mock data
+		AddDelegatorTxID: ids.ID{},
+		RewardUTXOs:      nil,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	myTransactor.EXPECT().ReportRewardUTXOs(ctx, [32]byte(utxoFetchedEvt.AddDelegatorTxID), mock.AnythingOfType("[]string")).Return(&types.Transaction{}, nil) // todo: mock data
+
+	myReceipter.EXPECT().TransactionReceipt(ctx, mock.AnythingOfType("common.Hash")).Return(&types.Receipt{Status: 1}, nil)
+
+	utxoFetchEvtObj := dispatcher.NewRootEventObject("StakingRewardUTXOFetcher", utxoFetchedEvt, ctx)
+
+	rewardUTXOsReportedEvt := &events.RewardUTXOsReportedEvent{}
+
+	myDispatcher.EXPECT().Publish(ctx, mock.AnythingOfType("*dispatcher.EventObject")).Run(func(ctx context.Context, evtObj *dispatcher.EventObject) {
+		rewardUTXOsReportedEvt = evtObj.Event.(*events.RewardUTXOsReportedEvent)
+	})
+
+	reporter := &StakingRewardUTXOReporter{
+		Logger:     logger.Default(),
+		Publisher:  myDispatcher,
+		Transactor: myTransactor,
+		Receipter:  myReceipter,
+	}
+
+	reporter.Do(ctx, utxoFetchEvtObj)
+
+	<-ctx.Done()
+
+	require.Equal(utxoFetchedEvt.AddDelegatorTxID, rewardUTXOsReportedEvt.AddDelegatorTxID)
+
+	myTransactor.AssertNumberOfCalls(suite.T(), " ReportRewardUTXOs", 1)
+	myReceipter.AssertNumberOfCalls(suite.T(), " TransactionReceipt", 1)
+	myDispatcher.AssertNumberOfCalls(suite.T(), "Publish", 1)
+}
+
+func TestStakeTaskCreatorTestSuite(t *testing.T) {
+	suite.Run(t, new(StakingRewardUTXOReporterTestSuite))
+}
