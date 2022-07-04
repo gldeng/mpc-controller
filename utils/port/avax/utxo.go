a08a7862292dc07facd99f0d8fd3d934f1526cfe
Zealy-Rockx <jigao@rockx.com>
2022-07-04 18:14:35 +0800

    Add UTXOsFromTransferableOutputs util.

--- utils/port/avax/utxo.go
+++ utils/port/avax/utxo.go
@@ -1,12 +1,29 @@
 package avax
 
 import (
+	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
-	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 )
 
-type UTXO struct {
-	avax.UTXOID
-	avax.Asset
-	Out *secp256k1fx.TransferOutput
+func UTXOsFromTransferableOutputs(txID ids.ID, outputs []*avax.TransferableOutput) []*avax.UTXO {
+	var utxos []*avax.UTXO
+	for index, output := range outputs {
+		utxo := UTXOFromTransferableOutput(txID, output, uint32(index))
+		utxos = append(utxos, utxo)
+	}
+
+	return utxos
+}
+
+func UTXOFromTransferableOutput(txID ids.ID, output *avax.TransferableOutput, outputIndex uint32) *avax.UTXO {
+	utxo := avax.UTXO{
+		UTXOID: avax.UTXOID{
+			TxID:        txID,
+			OutputIndex: outputIndex,
+		},
+		Asset: output.Asset,
+		Out:   output.Out,
+	}
+
+	return &utxo
 }
