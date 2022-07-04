d594228a6a553b5819901609c3296d6e12913c0b
Zealy-Rockx <jigao@rockx.com>
2022-07-04 15:34:09 +0800

    Improve UnsignedImportTx function.

--- utils/port/txs/cchain/unsigned_import_tx.go
+++ utils/port/txs/cchain/unsigned_import_tx.go
@@ -3,7 +3,6 @@ package cchain
 import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
-	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
 	"github.com/ava-labs/coreth/plugin/evm"
 	myAvax "github.com/avalido/mpc-controller/utils/port/avax"
 	"github.com/ethereum/go-ethereum/common"
@@ -15,26 +14,11 @@ type Args struct {
 	ChainID     ids.ID         // chain to import from
 	To          common.Address // Address of recipient
 	BaseFee     *big.Int       // fee to use post-AP3 // todo: consider this kind of fee
-	AtomicUTXOs []*myAvax.UTXO // UTXOs to spend
+	AtomicUTXOs []*avax.UTXO   // UTXOs to spend
 }
 
 func UnsignedImportTx(args *Args) *evm.UnsignedImportTx {
-	importedInputs := []*avax.TransferableInput{}
-
 	importedAmount := make(map[ids.ID]uint64)
-	for _, utxo := range args.AtomicUTXOs {
-		importedInputs = append(importedInputs, &avax.TransferableInput{
-			UTXOID: utxo.UTXOID,
-			Asset:  utxo.Asset,
-			In: &secp256k1fx.TransferInput{
-				Amt: utxo.Out.Amt, // todo: to adjust it to import fee
-				Input: secp256k1fx.Input{
-					SigIndices: []uint32{0}, // todo: to adjust?
-				},
-			},
-		})
-	}
-
 	outs := make([]evm.EVMOutput, 0, len(importedAmount))
 	for assetID, amount := range importedAmount {
 		outs = append(outs, evm.EVMOutput{
@@ -48,7 +32,7 @@ func UnsignedImportTx(args *Args) *evm.UnsignedImportTx {
 		NetworkID:      args.NetworkID,
 		BlockchainID:   args.ChainID,
 		Outs:           outs,
-		ImportedInputs: importedInputs,
+		ImportedInputs: myAvax.TransferableInputsrFromUTXOs(args.AtomicUTXOs),
 		SourceChain:    args.ChainID,
 	}
 	return utx
