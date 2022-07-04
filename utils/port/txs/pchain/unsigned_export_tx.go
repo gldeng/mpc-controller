c11fdd41a0bd3f9e656d5b64db34a4c73f339fdc
Zealy-Rockx <jigao@rockx.com>
2022-07-04 15:59:39 +0800

    Add some useful comments.

--- utils/port/txs/pchain/unsigned_export_tx.go
+++ utils/port/txs/pchain/unsigned_export_tx.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Args struct {
-	NetworkID          uint32
-	BlockchainID       ids.ID
-	DestinationChainID ids.ID
+	NetworkID          uint32 // ID of the network this chain lives on
+	BlockchainID       ids.ID // ID of the chain on which this transaction exists (prevents replay attacks)
+	DestinationChainID ids.ID // Which chain to send the funds to
 	Amount             uint64
 	To                 ids.ShortID
 	UTXOs              []*avax.UTXO // UTXOs to spend
@@ -22,10 +22,10 @@ func UnsignedExportTx(args *Args) *platformvm.UnsignedExportTx {
 		BaseTx: platformvm.BaseTx{BaseTx: avax.BaseTx{
 			NetworkID:    args.NetworkID,
 			BlockchainID: args.BlockchainID,
-			Ins:          myAvax.TransferableInputsrFromUTXOs(args.UTXOs),
+			Ins:          myAvax.TransferableInputsrFromUTXOs(args.UTXOs), // The inputs to this transaction
 		}},
 		DestinationChain: args.DestinationChainID,
-		ExportedOutputs: []*avax.TransferableOutput{{
+		ExportedOutputs: []*avax.TransferableOutput{{ // Outputs that are exported to the destination chain
 			Asset: args.UTXOs[0].Asset,
 			Out: &secp256k1fx.TransferOutput{
 				Amt: args.Amount,
