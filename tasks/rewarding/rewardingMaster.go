0658df20ef1406be3023c72321bc2dcb02201fc7
Zealy-Rockx <jigao@rockx.com>
2022-07-06 19:18:10 +0800

    [WIP]Update RewardingMaster.

--- tasks/rewarding/rewardingMaster.go
+++ tasks/rewarding/rewardingMaster.go
@@ -6,6 +6,7 @@ import (
 	"github.com/avalido/mpc-controller/dispatcher"
 	"github.com/avalido/mpc-controller/events"
 	"github.com/avalido/mpc-controller/logger"
+	"github.com/avalido/mpc-controller/tasks/rewarding/export"
 	"github.com/avalido/mpc-controller/tasks/rewarding/report"
 )
 
@@ -13,9 +14,18 @@ type RewardingMaster struct {
 	Logger           logger.Logger
 	RewardUTXOGetter chain.RewardUTXOGetter
 	Dispatcher       dispatcher.DispatcherClaasic
+	MyPubKeyHashHex  string
 
-	periodEndedChecker *report.StakingPeriodEndedChecker
-	rewardUTXOFetcher  *report.StakingRewardUTXOFetcher
+	// report
+	periodEndedChecker    *report.StakingPeriodEndedChecker
+	rewardUTXOFetcher     *report.StakingRewardUTXOFetcher
+	rewardedStakeReporter *report.RewardedStakeReporter
+
+	// export
+	exportRewardReqAddedEvtWatcher  *export.ExportRewardRequestAddedEventWatcher
+	exportRewardJoiner              *export.ExportRewardRequestJoiner
+	exportRewarReqStartedEvtWatcher *export.ExportRewardRequestStartedEventWatcher
+	rewardExporter                  *export.StakingRewardExporter
 }
 
 func (s *RewardingMaster) Start(ctx context.Context) error {
@@ -34,9 +44,31 @@ func (s *RewardingMaster) subscribe() {
 		Publisher:        s.Dispatcher,
 		RewardUTXOGetter: s.RewardUTXOGetter,
 	}
-
+	//
+	//rewardedStakeReporter := report.RewardedStakeReporter{
+	//	Logger:           s.Logger,
+	//	MyPubKeyHashHex string
+	//
+	//	Publisher:        s.Dispatcher,
+	//
+	//	Cache cache.ICache
+	//
+	//	Signer *bind.TransactOpts
+	//
+	//	ContractAddr common.Address
+	//	Transactor   bind.ContractTransactor
+	//
+	//	Receipter chain.Receipter
+	//}
+	//
 	s.periodEndedChecker = &periodEndedChecker
 	s.rewardUTXOFetcher = &rewardUTXOFetcher
+	//s.rewardUTXOFetcher =
+	//
+	//	s.exportRewardReqAddedEvtWatcher =
+	//		s.exportRewardJoiner =
+	//			s.exportRewarReqStartedEvtWatcher =
+	//				s.rewardExporter
 
 	s.Dispatcher.Subscribe(&events.StakingTaskDoneEvent{}, s.periodEndedChecker) // Emit event: *events.StakingPeriodEndedEvent
 
