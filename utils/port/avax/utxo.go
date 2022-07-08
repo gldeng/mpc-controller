9952a733c975cff541f49b308aa33179a737405c
Zealy-Rockx <jigao@rockx.com>
2022-07-08 17:36:06 +0800

    Add MpcUTXO type.

--- utils/port/avax/utxo.go
+++ utils/port/avax/utxo.go
@@ -3,7 +3,46 @@ package avax
 import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
+	"github.com/ava-labs/avalanchego/vms/secp256k1fx"
+	"github.com/jinzhu/copier"
 )
+
+type MpcUTXO struct {
+	UTXOID avax.UTXOID
+	Asset  avax.Asset
+	Out    *TransferOutput
+}
+
+type TransferOutput struct {
+	Amt          uint64
+	OutputOwners OutputOwners
+}
+
+type OutputOwners struct {
+	Locktime  uint64
+	Threshold uint32
+	Addrs     []ids.ShortID
+}
+
+func MpcUTXOFromUTXO(utxo *avax.UTXO) *MpcUTXO {
+	out := utxo.Out.(*secp256k1fx.TransferOutput)
+
+	outputOwners := OutputOwners{}
+	copier.Copy(&outputOwners, out.OutputOwners)
+
+	transferOutput := &TransferOutput{
+		Amt:          out.Amt,
+		OutputOwners: outputOwners,
+	}
+
+	mpcUTXO := &MpcUTXO{
+		UTXOID: utxo.UTXOID,
+		Asset:  utxo.Asset,
+		Out:    transferOutput,
+	}
+
+	return mpcUTXO
+}
 
 func UTXOsFromTransferableOutputs(txID ids.ID, outputs []*avax.TransferableOutput) []*avax.UTXO {
 	var utxos []*avax.UTXO
