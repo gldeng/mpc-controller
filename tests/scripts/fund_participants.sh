cecbb437164a7d838167ea5b9e229d4253311245
Zealy-Rockx <jigao@rockx.com>
2022-07-16 08:08:59 +0800

    Adjust staking period and mount for testings.

--- tests/scripts/fund_participants.sh
+++ tests/scripts/fund_participants.sh
@@ -8,6 +8,6 @@ ROLE_DEFAULT_ADMIN_PK="56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf5
 C_CHAIN_RPC_URL=http://127.0.0.1:9650/ext/bc/C/rpc
 
 # fund the participants so that they can afford gas fee
-cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value 0.001ether 0x3051bA2d313840932B7091D2e8684672496E9A4B > /dev/null
-cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value 0.001ether 0x7Ac8e2083E3503bE631a0557b3f2A8543EaAdd90 > /dev/null
-cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value 0.001ether 0x3600323b486F115CE127758ed84F26977628EeaA > /dev/null
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value 0.1ether 0x3051bA2d313840932B7091D2e8684672496E9A4B > /dev/null
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value 0.1ether 0x7Ac8e2083E3503bE631a0557b3f2A8543EaAdd90 > /dev/null
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK --value 0.1ether 0x3600323b486F115CE127758ed84F26977628EeaA > /dev/null
