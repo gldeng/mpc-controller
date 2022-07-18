f978bd23b76be8cbff460b25d0ec4c24786eed85
Zealy-Rockx <jigao@rockx.com>
2022-07-18 10:13:38 +0800

    Increase sleep duration.

--- tests/scripts/loop_initiate_stake.sh
+++ tests/scripts/loop_initiate_stake.sh
@@ -5,7 +5,7 @@ task tests:initiateStake
 while true
 do
 	  bash ./tests/scripts/fund_initiateStake.sh
-    sleep 10
-    venom run tests/testsuites/initiateStake.yml
     sleep 30
+    venom run tests/testsuites/initiateStake.yml
+    sleep 60
 done
