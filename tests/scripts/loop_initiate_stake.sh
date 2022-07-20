60eca56d0037da06ee21ec5174b3eadb7a3c6999
Zealy-Rockx <jigao@rockx.com>
2022-07-20 18:02:55 +0800

    Print loop initiateStake times.

--- tests/scripts/loop_initiate_stake.sh
+++ tests/scripts/loop_initiate_stake.sh
@@ -2,10 +2,14 @@
 
 task tests:initiateStake
 
+LOOP_INITIATE_STAKE=0
+
 while true
 do
 	  bash ./tests/scripts/fund_initiateStake.sh
     sleep 30
     venom run tests/testsuites/initiateStake.yml
     sleep 300
+    LOOP_INITIATE_STAKE=$((LOOP_INITIATE_STAKE+1))
+    echo loop initiateStake $LOOP_INITIATE_STAKE times
 done
