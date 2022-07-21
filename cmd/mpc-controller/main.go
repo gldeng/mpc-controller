53ff3ea1e2567e31589bc5a8a53382186b2bd17d
Zealy-Rockx <jigao@rockx.com>
2022-07-21 12:00:51 +0800

    Move Todo List.

--- cmd/mpc-controller/main.go
+++ cmd/mpc-controller/main.go
@@ -62,3 +62,22 @@ func main() {
 		fmt.Printf("Failed to run mpc-controller, error: %+v", err)
 	}
 }
+
+//## Todo List
+//- enhanced keystore to strength private key security
+//- automatic panic recover
+//- distributed trace, log and monitor
+//- deal with casual error: invalid nonce and nonce misused
+//- check and sync participant upon startup, there ere maybe groups created during mpc-controller downtime.
+//- add mpc-controller version info
+//- mechanism to check result from mpc-server and resume task on mpc-controller startup
+//- history even track for mpc-coordinator smart contract.
+//- log rotation with lumberjack: https://github.com/natefinch/lumberjack
+//- add main_test.go
+//- apply confluentinc/bincover: https://github.com/confluentinc/bincover
+//- restore data on startup
+//- automate tracking balance of addresses that receive principal and reward.
+//- take measures to deal with failed tasks
+//- take measures to avoid double-spend, maybe introduce SPE(single-participant-execution) strategy or consensus
+//- take measures to deal with package lost and disorder arrival
+//- ...
