7285c410ff7c2300fa3c506f8ab5a30a2033fa6d
Zealy-Rockx <jigao@rockx.com>
2022-07-21 12:08:18 +0800

    Comment out mock mpc_server url.

--- tests/scripts/start_mpc-controller.sh
+++ tests/scripts/start_mpc-controller.sh
@@ -13,8 +13,8 @@ mkdir -p configs
 mkdir -p logs
 
 sks=("3db317d8f1ff081a32038c339901f8a6a15f53122dde3b99fa8017a2d0952f5ae5cdf5d0df912b8fc7755776a3b12af8cb7aece27b721d7f2c13b4cc957d1ff40131c7b18472748f2aeff2cf9a8aa46539d93281d367bdbe37179955824810ba" "49b5078e82a8ac2da6493fcc8da4ab8d97b3e2ca85e51b0d1fb5d271d4eaa5932a74784ce7672827d7f0ff4aeae1e73cadce9bf0ea75d6dc4eaab7ac8c81cb4a1c034f40d11940ffd65822a9d34f41830303516ac600e42b5e98ccf980efb67e" "f4c6214f7a5ec30236b9aaa2cddfc0963a4fafe52b29e2a2f0cf2c246de8fb11546d38591e2e45b9d9076bf6739282dce52b8f93651668875c784a1083adf4ae2af1253372ea921a1e5795975e372e829de2a1753e2d0bafcf760ef984881204")
-#MPC_SERVER_URLS=("http://localhost:8001" "http://localhost:8002" "http://localhost:8003")
-MPC_SERVER_URLS=("http://localhost:9000" "http://localhost:9000" "http://localhost:9000")
+MPC_SERVER_URLS=("http://localhost:8001" "http://localhost:8002" "http://localhost:8003")
+#MPC_SERVER_URLS=("http://localhost:9000" "http://localhost:9000" "http://localhost:9000")
 
 
 MPC_MANAGER_ADDRESS=$(cat $HOME/mpctest/contracts/addresses/MPC_MANAGER_ADDRESS)
