1268cb5f289c85149aecabe63a2f568d49df258f
Zealy-Rockx <jigao@rockx.com>
2022-07-22 17:48:18 +0800

    Add script for mpc-server-mock.

---
+++ tests/scripts/start_mpc-server_mock.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+LAST_WD=$(pwd)
+
+echo "Start mpc-server-mock"
+
+read LAST_TEST_WD < $HOME/mpctest/testwd_last
+
+cd $LAST_TEST_WD/mpc-server-mock
+
+mkdir log
+
+$HOME/mpctest/mpc-server-mock/mpc_server -p 7 - t 4 > log/log.log 2>&1 &
+
+cd $LAST_WD
