dee1c9b2e83804b9ea44b8a996cbdb2b56f307d0
Zealy-Rockx <jigao@rockx.com>
2022-07-22 19:32:23 +0800

    Use threshold variable.

--- contract/abigen.go
+++ contract/abigen.go
@@ -2,3 +2,7 @@
 //go:generate abigen --sol src/MpcManager.sol --pkg contract --out MpcManager.go --type MpcManager
 
 package contract
+
+// Compatible abigen version: 1.10.17-stable. Source code download links:
+// https://github.com/ethereum/go-ethereum/archive/refs/tags/v1.10.17.zip
+// https://github.com/ethereum/go-ethereum/archive/refs/tags/v1.10.17.tar.gz
