267cfbad0df9fee8d40629f1be351ca9746b94cd
Zealy-Rockx <jigao@rockx.com>
2022-07-23 23:44:32 +0800

    Trim new line suffix.

--- logger/adapter/badgerDB.go
+++ logger/adapter/badgerDB.go
@@ -3,6 +3,7 @@ package adapter
 import (
 	"fmt"
 	"github.com/avalido/mpc-controller/logger"
+	"strings"
 )
 
 var _ logger.BadgerDBLogger = (*BadgerDBLoggerAdapter)(nil)
@@ -12,17 +13,21 @@ type BadgerDBLoggerAdapter struct {
 }
 
 func (l *BadgerDBLoggerAdapter) Debugf(format string, a ...interface{}) {
-	l.Debug(fmt.Sprintf(format, a...))
+	msg := strings.TrimSuffix(fmt.Sprintf(format, a...), "\n")
+	l.Debug(msg)
 }
 
 func (l *BadgerDBLoggerAdapter) Infof(format string, a ...interface{}) {
-	l.Info(fmt.Sprintf(format, a...))
+	msg := strings.TrimSuffix(fmt.Sprintf(format, a...), "\n")
+	l.Info(msg)
 }
 
 func (l *BadgerDBLoggerAdapter) Warningf(format string, a ...interface{}) {
-	l.Warn(fmt.Sprintf(format, a...))
+	msg := strings.TrimSuffix(fmt.Sprintf(format, a...), "\n")
+	l.Warn(msg)
 }
 
 func (l *BadgerDBLoggerAdapter) Errorf(format string, a ...interface{}) {
-	l.Error(fmt.Sprintf(format, a...))
+	msg := strings.TrimSuffix(fmt.Sprintf(format, a...), "\n")
+	l.Error(msg)
 }
