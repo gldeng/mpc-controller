70e3b29c8fa5b315fa53b1a3b448be94177c4674
Zealy-Rockx <jigao@rockx.com>
2022-07-23 21:38:30 +0800

    Apply BadgerDBLoggerAdapter.

--- storage/badgerDB/db.go
+++ storage/badgerDB/db.go
@@ -1,11 +1,15 @@
 package badgerDB
 
 import (
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/avalido/mpc-controller/logger/adapter"
 	"github.com/dgraph-io/badger/v3"
 )
 
-func NewBadgerDBWithDefaultOptions(path string) *badger.DB {
-	db, err := badger.Open(badger.DefaultOptions(path))
+func NewBadgerDBWithDefaultOptions(path string, logger logger.Logger) *badger.DB {
+	opt := badger.DefaultOptions(path)
+	opt.Logger = &adapter.BadgerDBLoggerAdapter{StdLogger: &adapter.StdLoggerAdapter{Logger: logger}}
+	db, err := badger.Open(opt)
 	if err != nil {
 		panic("Failed to open badger database")
 	}
