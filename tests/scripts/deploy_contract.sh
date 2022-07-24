1a0c596415949f69708d833b4c60e45fba6f9ce8
Zealy-Rockx <jigao@rockx.com>
2022-07-24 08:58:50 +0800

    Remove cached.

--- tests/scripts/deploy_contract.sh
+++ tests/scripts/deploy_contract.sh
@@ -22,6 +22,7 @@ echo "Start deploying smart contracts"
 cd $HOME/mpctest/contracts/
 
 rm -rf ./out
+rm -rf ./cache
 
 # Deploy MpcManager contract
 MPC_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK MpcManager | grep -i "deployed" | cut -d " " -f 3)
