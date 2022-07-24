6d299acb0ba39a80b31ea6ad835a984e01e39ce2
Zealy-Rockx <jigao@rockx.com>
2022-07-24 09:29:31 +0800

    Tune sleep to 5 seconds.

--- tests/scripts/loop_initiate_stake.sh
+++ tests/scripts/loop_initiate_stake.sh
@@ -7,11 +7,11 @@ echo Starting loop initiateStake request
 while true
 do
     bash ./tests/scripts/fund_participants.sh
-    sleep 10
+    sleep 5
 	  bash ./tests/scripts/fund_initiateStake.sh
-    sleep 10
+    sleep 5
     venom run tests/testsuites/initiateStake.yml
     LOOP_INITIATE_STAKE=$((LOOP_INITIATE_STAKE+1))
     echo Looped initiateStake at $(date +%Y-%m-%d/%H:%M:%S), total times: $LOOP_INITIATE_STAKE
-    sleep 300
+    sleep 5
 done
