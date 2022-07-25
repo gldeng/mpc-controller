ca2990b1ebe7058a3cfdd8c5d0b0361db8f308af
Zealy-Rockx <jigao@rockx.com>
2022-07-25 14:25:25 +0800

    Improve error msg.

--- utils/crypto/secp256k1r/verifier.go
+++ utils/crypto/secp256k1r/verifier.go
@@ -3,6 +3,7 @@ package secp256k1r
 import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/crypto"
+	"github.com/avalido/mpc-controller/utils/bytes"
 	"github.com/pkg/errors"
 )
 
@@ -14,11 +15,11 @@ type Verifier struct {
 func (v *Verifier) VerifySig(hash []byte, sig [65]byte) (bool, error) {
 	pk, err := v.RecoverHashPublicKey(hash, sig[:])
 	if err != nil {
-		return false, errors.WithStack(err)
+		return false, errors.Wrapf(err, "failed to recover public key with hash %q and signature %q", bytes.BytesToHex(hash), bytes.Bytes65ToHex(sig))
 	}
 
 	if v.PChainAddress != pk.Address() {
-		return false, errors.New("VerifySig:invalid signature")
+		return false, errors.Errorf("expected P-Chain address is %q, but got %q", v.PChainAddress, pk.Address())
 	}
 
 	return true, nil
