303704068db532f32644dc9f79c8675f4c74c095
Zealy-Rockx <jigao@rockx.com>
2022-07-25 18:22:49 +0800

    Add lnavAvalanche task.

--- tests/scripts/start_mpc-server.sh
+++ tests/scripts/start_mpc-server.sh
@@ -9,7 +9,7 @@ read LAST_TEST_WD < $HOME/mpctest/testwd_last
 cd $LAST_TEST_WD/mpc-server
 
 mkdir secret
-mkdir log
+mkdir logs
 mkdir db
 
 MPC_SERVER_REPO=$HOME/mpctest/mpc-server
@@ -23,12 +23,12 @@ echo -n "b17eac91d7aa2bd5fa72916b6c8a35ab06e8f0c325c98067bbc9645b85ce789f" > sec
 echo -n "7084300e7059ea4b308ec5b965ef581d3f9c9cd63714082ccf9b9d1fb34d658b" > secret/p7.s
 
 RUST_BACKTRACE=full $MPC_SERVER_REPO/messenger/target/debug/messenger > log/messenger.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p1.s --port 8001 -m http://127.0.0.1:8000  --db-path db/p1.db > log/p1.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p2.s --port 8002 -m http://127.0.0.1:8000  --db-path db/p2.db > log/p2.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p3.s --port 8003 -m http://127.0.0.1:8000  --db-path db/p3.db > log/p3.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p4.s --port 8004 -m http://127.0.0.1:8000  --db-path db/p4.db > log/p4.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p5.s --port 8005 -m http://127.0.0.1:8000  --db-path db/p5.db > log/p5.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p6.s --port 8006 -m http://127.0.0.1:8000  --db-path db/p6.db > log/p6.log 2>&1 &
-RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p7.s --port 8007 -m http://127.0.0.1:8000  --db-path db/p7.db > log/p7.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p1.s --port 8001 -m http://127.0.0.1:8000  --db-path db/p1.db > logs/p1.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p2.s --port 8002 -m http://127.0.0.1:8000  --db-path db/p2.db > logs/p2.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p3.s --port 8003 -m http://127.0.0.1:8000  --db-path db/p3.db > logs/p3.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p4.s --port 8004 -m http://127.0.0.1:8000  --db-path db/p4.db > logs/p4.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p5.s --port 8005 -m http://127.0.0.1:8000  --db-path db/p5.db > logs/p5.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p6.s --port 8006 -m http://127.0.0.1:8000  --db-path db/p6.db > logs/p6.log 2>&1 &
+RUST_BACKTRACE=full $MPC_SERVER_REPO/target/debug/mpc-server -s secret/p7.s --port 8007 -m http://127.0.0.1:8000  --db-path db/p7.db > logs/p7.log 2>&1 &
 
 cd $LAST_WD
