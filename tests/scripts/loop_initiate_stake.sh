d5ed8411b5f74412e09bc05b21fc5f3c0dbc8ed6
Zealy-Rockx <jigao@rockx.com>
2022-07-26 08:37:26 +0800

    Add max loops allowed.

--- tests/scripts/loop_initiate_stake.sh
+++ tests/scripts/loop_initiate_stake.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 LOOP_INITIATE_STAKE=0
+MAX_LOOPS_ALLOWED=1000 # todo: enable command line config
 
 echo Starting loop initiateStake request
 
-while true
+while [ $LOOP_INITIATE_STAKE -le $MAX_LOOPS_ALLOWED ]
 do
     bash ./tests/scripts/fund_participants.sh
-#    sleep 5
 	  bash ./tests/scripts/fund_initiateStake.sh
-#    sleep 5
+
     venom run tests/testsuites/initiateStake.yml
     LOOP_INITIATE_STAKE=$((LOOP_INITIATE_STAKE+1))
+
     echo Looped initiateStake at $(date +%Y-%m-%d/%H:%M:%S), total times: $LOOP_INITIATE_STAKE
-    sleep 15
 done
