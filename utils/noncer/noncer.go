31e97dc065ad403e99d9004c1dedf47031223b65
Zealy-Rockx <jigao@rockx.com>
2022-07-27 23:19:50 +0800

    Improve noncer.

--- utils/noncer/noncer.go
+++ utils/noncer/noncer.go
@@ -6,12 +6,13 @@ import (
 
 type Noncer interface {
 	GetNonce(reqID uint64) (nonce uint64)
-	ResetBase(baseReqID, baseNonce uint64)
+	ResetBase(baseReqID, baseNonce uint64) bool
 }
 
 type noncer struct {
 	baseReqID uint64
 	baseNonce uint64
+	gap       int64
 	lock      *sync.Mutex
 }
 
@@ -19,20 +20,27 @@ func New(baseReqID, baseNonce uint64) Noncer {
 	return &noncer{
 		baseReqID: baseReqID,
 		baseNonce: baseNonce,
+		gap:       int64(baseReqID - baseNonce),
 		lock:      new(sync.Mutex),
 	}
 }
 
 func (n *noncer) GetNonce(reqID uint64) (nonce uint64) {
 	n.lock.Lock()
-	nonce = n.baseNonce + (reqID - n.baseReqID)
-	n.lock.Unlock()
-	return
+	defer n.lock.Unlock()
+	nonceInt64 := int64(reqID) - n.gap
+	return uint64(nonceInt64)
 }
 
-func (n *noncer) ResetBase(baseReqID, baseNonce uint64) {
+func (n *noncer) ResetBase(baseReqID, baseNonce uint64) bool {
 	n.lock.Lock()
+	defer n.lock.Unlock()
+	newGap := int64(baseReqID - baseNonce)
+	if newGap == n.gap {
+		return false
+	}
 	n.baseReqID = baseReqID
 	n.baseNonce = baseNonce
-	n.lock.Unlock()
+	n.gap = newGap
+	return true
 }
