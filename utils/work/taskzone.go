9b5f677e1cc89aa4fa375ffe4f7a1a30f2047ccc
Zealy-Rockx <jigao@rockx.com>
2022-07-31 19:37:20 +0800

    Seperate health check.

--- utils/work/taskzone.go
+++ utils/work/taskzone.go
@@ -5,6 +5,7 @@ import (
 	"github.com/avalido/mpc-controller/logger"
 	"github.com/avalido/mpc-controller/utils/queue"
 	"sync"
+	"time"
 )
 
 type Task struct {
@@ -28,6 +29,7 @@ type TaskZone struct {
 func (z *TaskZone) Run(ctx context.Context) {
 	z.once.Do(func() {
 		z.taskPriorityQueues = make(map[int]queue.Queue)
+		go z.run(ctx)
 	})
 	for {
 		select {
@@ -42,17 +44,39 @@ func (z *TaskZone) Run(ctx context.Context) {
 	}
 }
 
-func (z *TaskZone) EnZone(t *Task) error {
-	defer func() {
-		z.Logger.Debug(z.Id+" en-zoned tasks stats in priority", []logger.Field{
-			{"p5", z.tasksInQueue(5)},
-			{"p4", z.tasksInQueue(4)},
-			{"p3", z.tasksInQueue(3)},
-			{"p2", z.tasksInQueue(2)},
-			{"p1", z.tasksInQueue(1)},
-			{"p0", z.tasksInQueue(0)}}...)
-	}()
+func (z *TaskZone) run(ctx context.Context) {
+	t := time.NewTicker(time.Minute * 5)
+	defer t.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-t.C:
+			var totalTasks int
+			var totalCap int
+			for _, q := range z.taskPriorityQueues {
+				totalTasks = totalTasks + q.Count()
+				totalCap = totalCap + q.Capacity()
+			}
 
+			isTooMany := float64(totalTasks) > float64(totalCap)*0.8
+
+			z.Logger.WarnOnTrue(isTooMany, z.Id+" too many en-zoned tasks",
+				[]logger.Field{{"totalTasks", totalTasks},
+					{"maxTasks", totalCap}}...)
+
+			z.Logger.DebugOnTrue(isTooMany, z.Id+" en-zoned tasks stats in priority", []logger.Field{
+				{"p5", z.tasksInQueue(5)},
+				{"p4", z.tasksInQueue(4)},
+				{"p3", z.tasksInQueue(3)},
+				{"p2", z.tasksInQueue(2)},
+				{"p1", z.tasksInQueue(1)},
+				{"p0", z.tasksInQueue(0)}}...)
+		}
+	}
+}
+
+func (z *TaskZone) EnZone(t *Task) error {
 	_, ok := z.taskPriorityQueues[t.Priority]
 	if !ok {
 		z.taskPriorityQueues[t.Priority] = queue.NewArrayQueue(z.PerTaskQueueSize)
