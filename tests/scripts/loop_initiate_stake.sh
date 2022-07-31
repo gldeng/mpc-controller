3785277f09529b99d8c9ae5e8ab0111e757e418f
Zealy-Rockx <jigao@rockx.com>
2022-07-31 22:35:33 +0800

    Comment out fund and sleep.

--- tests/scripts/loop_initiate_stake.sh
+++ tests/scripts/loop_initiate_stake.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 LOOP_INITIATE_STAKE=0
-MAX_LOOPS_ALLOWED=10000 # todo: enable command line config
+MAX_LOOPS_ALLOWED=100 # todo: enable command line config
 
 echo Starting loop initiateStake request
 
 while [ $LOOP_INITIATE_STAKE -lt $MAX_LOOPS_ALLOWED ]
 do
 #    bash ./tests/scripts/fund_participants.sh
-	  bash ./tests/scripts/fund_initiateStake.sh
+#	  bash ./tests/scripts/fund_initiateStake.sh
 
     venom run tests/testsuites/initiateStake.yml
     LOOP_INITIATE_STAKE=$((LOOP_INITIATE_STAKE+1))
 
     echo Looped initiateStake at $(date +%Y-%m-%d/%H:%M:%S), total times: $LOOP_INITIATE_STAKE
-    sleep 26
+#    sleep 23
 done
