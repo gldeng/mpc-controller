7427c009e2a9a930a748cf683b72f6ad6b94dbe3
Zealy-Rockx <jigao@rockx.com>
2022-08-01 12:05:50 +0800

    Add promautos.

---
+++ promth/promauto.go
@@ -0,0 +1,52 @@
+package promth
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	// stake
+
+	StakeRequestAdded = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "stake_request_added_total",
+		Help: "The total number of StakeRequestAddedEvent",
+	})
+	StakeTaskDone = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "stake_task_done_total",
+		Help: "The total number of StakeTaskDoneEvent",
+	})
+
+	// reward
+
+	PrincipalUTXOExported = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "principal_utxo_exported_total",
+		Help: "The total number of PrincipalUTXOExportedEvent",
+	})
+	RewardUTXOExported = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "reward_utxo_exported_total",
+		Help: "The total number of RewardUTXOExportedEvent",
+	})
+
+	// sign
+
+	PrincipalUTXOSignTaskAdded = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "principal_utxo_sign_task_added_total",
+		Help: "The total number of principal UTXO sign task added",
+	})
+
+	RewardUTXOSignTaskAdded = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "reward_utxo_sign_task_added_total",
+		Help: "The total number of reward UTXO sign task added",
+	})
+
+	PrincipalUTXOSignTaskDone = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "principal_utxo_sign_task_done_total",
+		Help: "The total number of of principal UTXO sign task done",
+	})
+
+	RewardUTXOSignTaskDone = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "reward_utxo_sign_task_done_total",
+		Help: "The total number of reward UTXO sign task done",
+	})
+)
