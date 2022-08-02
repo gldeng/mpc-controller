63cbc3840056fdaadabfd8409b52abc9447699bb
Zealy-Rockx <jigao@rockx.com>
2022-08-02 18:23:26 +0800

    Add MpcManager emitted event types.

--- events/contract.go
+++ events/contract.go
@@ -3,6 +3,7 @@ package events
 import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"math/big"
 )
 
@@ -29,4 +30,40 @@ type JoinedRequestEvent struct {
 	TxHashHex  string
 	RequestId  *big.Int
 	PartiIndex *big.Int
+}
+
+// MpcManager emitted events
+
+type ParticipantAddedEvent struct { // event ParticipantAdded(bytes indexed publicKey, bytes32 groupId, uint256 index)
+	PublicKey common.Hash // indexed
+	GroupId   [32]byte
+	Index     *big.Int
+	Raw       *types.Log
+}
+
+type KeygenRequestAdded struct { // event KeyGenerated(bytes32 indexed groupId, bytes publicKey)
+	GroupId [32]byte // indexed
+	Raw     *types.Log
+}
+
+type KeyGeneratedEvent struct { // event KeygenRequestAdded(bytes32 indexed groupId)
+	GroupId   [32]byte // indexed
+	PublicKey []byte
+	Raw       *types.Log
+}
+
+type StakeRequestAddedEvent struct { // event StakeRequestAdded(uint256 requestNumber, bytes indexed publicKey, string nodeID, uint256 amount, uint256 startTime, uint256 endTime)
+	RequestNumber *big.Int
+	PublicKey     common.Hash // indexed
+	NodeID        string
+	Amount        *big.Int
+	StartTime     *big.Int
+	EndTime       *big.Int
+	Raw           *types.Log
+}
+
+type RequestStartedEvent struct { // event RequestStarted(bytes32 indexed requestHash, uint256 participantIndices)
+	RequestHash        [32]byte // indexed
+	ParticipantIndices *big.Int
+	Raw                *types.Log
 }
