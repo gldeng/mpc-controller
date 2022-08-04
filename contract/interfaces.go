b64abbcee173d54ff6fef7891c67b5b95a3d2e6b
Zealy-Rockx <jigao@rockx.com>
2022-08-04 18:28:42 +0800

    Simplify interfaces.

--- contract/interfaces.go
+++ contract/interfaces.go
@@ -1,9 +1,9 @@
 package contract
 
 import (
-	"context"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"math/big"
 )
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -11,58 +11,16 @@ import (
 
 // Callers
 
-type CallerGetGroup interface {
-	GetGroup(ctx context.Context, groupId [32]byte) (Participants [][]byte, Threshold *big.Int, err error)
+type Caller interface {
+	GetGroup(opts *bind.CallOpts, groupId [32]byte) ([][]byte, error)
+	GetGroupIdByKey(opts *bind.CallOpts, publicKey []byte) ([32]byte, error)
+	PrincipalTreasuryAddress(opts *bind.CallOpts) (common.Address, error)
+	RewardTreasuryAddress(opts *bind.CallOpts) (common.Address, error)
 }
 
 // Transactor
 
-type TransactorJoinRequest interface {
-	JoinRequest(ctx context.Context, requestId *big.Int, myIndex *big.Int) (*types.Transaction, error)
-}
-
-type TransactorReportGeneratedKey interface {
-	ReportGeneratedKey(ctx context.Context, groupId [32]byte, myIndex *big.Int, generatedPublicKey []byte) (*types.Transaction, error)
-}
-
-type TransactorReportUTXO interface {
-	ReportUTXO(ctx context.Context, groupId [32]byte, myIndex *big.Int, genPubKey []byte, utxoTxID [32]byte, utxoOutputIndex uint32) (*types.Transaction, error)
-}
-
-//type TransactorJoinExportUTXO interface {
-//	JoinExportUTXO(ctx context.Context, groupId [32]byte, myIndex *big.Int, genPubKey []byte, utxoTxID [32]byte, utxoOutputIndex uint32) (*types.Transaction, error)
-//}
-
-// Filters
-
-type FilterParticipantAdded interface {
-	WatchParticipantAdded(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerParticipantAdded, error)
-}
-
-type FilterKeygenRequestAdded interface {
-	WatchKeygenRequestAdded(ctx context.Context, groupId [][32]byte) (<-chan *MpcManagerKeygenRequestAdded, error)
-}
-
-type FilterKeyGenerated interface {
-	WatchKeyGenerated(ctx context.Context, groupId [][32]byte) (<-chan *MpcManagerKeyGenerated, error)
-}
-
-type FilterStakeRequestAdded interface {
-	WatchStakeRequestAdded(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerStakeRequestAdded, error)
-}
-
-type FilterStakeRequestStarted interface {
-	WatchStakeRequestStarted(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerStakeRequestStarted, error)
-}
-
-//type FilterExportUTXORequestAdded interface {
-//	WatchExportUTXORequestAdded(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerExportUTXORequestAdded, error)
-//}
-//
-//type FilterExportUTXORequestStarted interface {
-//	WatchExportUTXORequestStarted(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerExportUTXORequestStarted, error)
-//}
-
-type FilterExportUTXORequest interface {
-	WatchExportUTXORequest(ctx context.Context, publicKey [][]byte) (<-chan *MpcManagerExportUTXORequest, error)
+type Transactor interface {
+	JoinRequest(opts *bind.TransactOpts, participantId [32]byte, requestHash [32]byte) (*types.Transaction, error)
+	ReportGeneratedKey(opts *bind.TransactOpts, participantId [32]byte, generatedPublicKey []byte) (*types.Transaction, error)
 }
