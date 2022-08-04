cb8293b5c13c43ab262fa341722d11752a8c8008
Zealy-Rockx <jigao@rockx.com>
2022-08-04 11:59:35 +0800

    Introduce BoundFilter.

--- contract/watcher/factory.go
+++ contract/watcher/factory.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/avalido/mpc-controller/contract"
 	"github.com/avalido/mpc-controller/logger"
+	myBind "github.com/avalido/mpc-controller/utils/contract/bind"
 	"github.com/avalido/mpc-controller/utils/contract/watcher"
+
 	"github.com/avalido/mpc-controller/utils/dispatcher"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -25,7 +27,7 @@ type EvtName string
 type Factory struct {
 	Logger        logger.Logger
 	Publisher     dispatcher.Publisher
-	BoundContract bind.BoundContract
+	BoundFilterer myBind.BoundFilterer
 }
 
 func (f *Factory) NewWatcher(process watcher.Process, opts *bind.WatchOpts, name EvtName, queries ...[]interface{}) (*watcher.Watcher, error) {
@@ -37,50 +39,50 @@ func (f *Factory) NewWatcher(process watcher.Process, opts *bind.WatchOpts, name
 	)
 	switch name {
 	case EvtParticipantAdded:
-		logs, sub, err = f.BoundContract.WatchLogs(opts, string(EvtParticipantAdded), queries...)
+		logs, sub, err = f.BoundFilterer.WatchLogs(opts, string(EvtParticipantAdded), queries...)
 		unpack = func(log types.Log) (interface{}, error) {
 			myEvent := new(contract.MpcManagerParticipantAdded)
-			if err := f.BoundContract.UnpackLog(myEvent, string(EvtParticipantAdded), log); err != nil {
+			if err := f.BoundFilterer.UnpackLog(myEvent, string(EvtParticipantAdded), log); err != nil {
 				return nil, errors.Wrapf(err, fmt.Sprintf("failed to unpack %v log", EvtParticipantAdded))
 			}
 			myEvent.Raw = log
 			return myEvent, nil
 		}
 	case EvtKeygenRequestAdded:
-		logs, sub, err = f.BoundContract.WatchLogs(opts, string(EvtKeygenRequestAdded), queries...)
+		logs, sub, err = f.BoundFilterer.WatchLogs(opts, string(EvtKeygenRequestAdded), queries...)
 		unpack = func(log types.Log) (interface{}, error) {
 			myEvent := new(contract.MpcManagerKeygenRequestAdded)
-			if err := f.BoundContract.UnpackLog(myEvent, string(EvtKeygenRequestAdded), log); err != nil {
+			if err := f.BoundFilterer.UnpackLog(myEvent, string(EvtKeygenRequestAdded), log); err != nil {
 				return nil, errors.Wrapf(err, fmt.Sprintf("failed to unpack %v log", EvtKeygenRequestAdded))
 			}
 			myEvent.Raw = log
 			return myEvent, nil
 		}
 	case EvtKeyGenerated:
-		logs, sub, err = f.BoundContract.WatchLogs(opts, string(EvtKeyGenerated), queries...)
+		logs, sub, err = f.BoundFilterer.WatchLogs(opts, string(EvtKeyGenerated), queries...)
 		unpack = func(log types.Log) (interface{}, error) {
 			myEvent := new(contract.MpcManagerKeyGenerated)
-			if err := f.BoundContract.UnpackLog(myEvent, string(EvtKeyGenerated), log); err != nil {
+			if err := f.BoundFilterer.UnpackLog(myEvent, string(EvtKeyGenerated), log); err != nil {
 				return nil, errors.Wrapf(err, fmt.Sprintf("failed to unpack %v log", EvtKeyGenerated))
 			}
 			myEvent.Raw = log
 			return myEvent, nil
 		}
 	case EvtStakeRequestAdded:
-		logs, sub, err = f.BoundContract.WatchLogs(opts, string(EvtStakeRequestAdded), queries...)
+		logs, sub, err = f.BoundFilterer.WatchLogs(opts, string(EvtStakeRequestAdded), queries...)
 		unpack = func(log types.Log) (interface{}, error) {
 			myEvent := new(contract.MpcManagerStakeRequestAdded)
-			if err := f.BoundContract.UnpackLog(myEvent, string(EvtStakeRequestAdded), log); err != nil {
+			if err := f.BoundFilterer.UnpackLog(myEvent, string(EvtStakeRequestAdded), log); err != nil {
 				return nil, errors.Wrapf(err, fmt.Sprintf("failed to unpack %v log", EvtStakeRequestAdded))
 			}
 			myEvent.Raw = log
 			return myEvent, nil
 		}
 	case EvtRequestStarted:
-		logs, sub, err = f.BoundContract.WatchLogs(opts, string(EvtRequestStarted), queries...)
+		logs, sub, err = f.BoundFilterer.WatchLogs(opts, string(EvtRequestStarted), queries...)
 		unpack = func(log types.Log) (interface{}, error) {
 			myEvent := new(contract.MpcManagerRequestStarted)
-			if err := f.BoundContract.UnpackLog(myEvent, string(EvtRequestStarted), log); err != nil {
+			if err := f.BoundFilterer.UnpackLog(myEvent, string(EvtRequestStarted), log); err != nil {
 				return nil, errors.Wrapf(err, fmt.Sprintf("failed to unpack %v log", EvtRequestStarted))
 			}
 			myEvent.Raw = log
