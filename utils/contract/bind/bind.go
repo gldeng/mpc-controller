5807279e9f89124863e9b715530019d5ad05f3b5
Zealy-Rockx <jigao@rockx.com>
2022-08-04 11:54:13 +0800

    Add BoundContract.

--- utils/contract/bind/bind.go
+++ utils/contract/bind/bind.go
@@ -4,9 +4,34 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
 	"github.com/pkg/errors"
 	"strings"
 )
+
+type BoundContract interface {
+	BoundCaller
+	BoundTransactor
+	BoundFilterer
+}
+
+type BoundCaller interface {
+	Call(opts *bind.CallOpts, results *[]interface{}, method string, params ...interface{}) error
+}
+
+type BoundTransactor interface {
+	Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error)
+	RawTransact(opts *bind.TransactOpts, calldata []byte) (*types.Transaction, error)
+	Transfer(opts *bind.TransactOpts) (*types.Transaction, error)
+}
+
+type BoundFilterer interface {
+	FilterLogs(opts *bind.FilterOpts, name string, query ...[]interface{}) (chan types.Log, event.Subscription, error)
+	WatchLogs(opts *bind.WatchOpts, name string, query ...[]interface{}) (chan types.Log, event.Subscription, error)
+	UnpackLog(out interface{}, event string, log types.Log) error
+	UnpackLogIntoMap(out map[string]interface{}, event string, log types.Log) error
+}
 
 type ABI func() string
 
