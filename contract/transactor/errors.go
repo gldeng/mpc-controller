11fcbdf55faac356955beb0ad39920f3264500ed
Zealy-Rockx <jigao@rockx.com>
2022-08-05 08:49:34 +0800

    Add errors.

---
+++ contract/transactor/errors.go
@@ -0,0 +1,44 @@
+package transactor
+
+import "fmt"
+
+const (
+	ErrMsgQuorumAlreadyReached = "quorum already reached"
+	ErrMsgAttemptToRejoin      = "attempt to rejoin"
+)
+
+// ----------error types ----------
+
+type ErrTypQuorumAlreadyReached struct {
+	ErrMsg string
+	Cause  error
+}
+
+func (e *ErrTypQuorumAlreadyReached) Error() string {
+	if e.ErrMsg == "" {
+		return ErrMsgQuorumAlreadyReached + fmt.Sprintf(":%v", e.Cause)
+	}
+	return e.ErrMsg + fmt.Sprintf(":%v", e.Cause)
+}
+
+func (e *ErrTypQuorumAlreadyReached) Unwrap() error {
+	return e.Cause
+}
+
+// ----------
+
+type ErrTypAttemptToRejoin struct {
+	ErrMsg string
+	Cause  error
+}
+
+func (e *ErrTypAttemptToRejoin) Error() string {
+	if e.ErrMsg == "" {
+		return ErrMsgAttemptToRejoin + fmt.Sprintf(":%v", e.Cause)
+	}
+	return e.ErrMsg + fmt.Sprintf(":%v", e.Cause)
+}
+
+func (e *ErrTypAttemptToRejoin) Unwrap() error {
+	return e.Cause
+}
