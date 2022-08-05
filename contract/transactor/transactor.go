c1ed768d6429130404a06dfb8d623d35f25e784b
Zealy-Rockx <jigao@rockx.com>
2022-08-05 10:23:36 +0800

    Add ReportGeneratedKey.

--- contract/transactor/transactor.go
+++ contract/transactor/transactor.go
@@ -24,8 +24,8 @@ const (
 type Method string
 
 type Transactor interface {
-	JoinRequest(ctx context.Context, participantId [32]byte, requestHash [32]byte) (*types.Transaction, *types.Receipt, error)
-	ReportGeneratedKey(ctx context.Context, participantId [32]byte, generatedPublicKey []byte) (*types.Transaction, *types.Receipt, error)
+	JoinRequest(ctx context.Context, partiId [32]byte, reqHash [32]byte) (*types.Transaction, *types.Receipt, error)
+	ReportGeneratedKey(ctx context.Context, partiId [32]byte, genPubKey []byte) (*types.Transaction, *types.Receipt, error)
 }
 
 type MyTransactor struct {
@@ -60,9 +60,9 @@ func (t *MyTransactor) JoinRequest(ctx context.Context, participantId [32]byte, 
 		return false, nil
 	})
 
-	participantIdHex := bytes.Bytes32ToHex(participantId)
-	requestHashHex := bytes.Bytes32ToHex(requestHash)
-	err = errors.Wrapf(err, "failed to join request. participantId:%v, requestHash:%v", participantIdHex, requestHashHex)
+	partiIdHex := bytes.Bytes32ToHex(participantId)
+	reqHashHex := bytes.Bytes32ToHex(requestHash)
+	err = errors.Wrapf(err, "failed to join request. partiId:%v, reqHash:%v", partiIdHex, reqHashHex)
 	if err != nil {
 		return tx, nil, err
 	}
@@ -81,7 +81,7 @@ func (t *MyTransactor) JoinRequest(ctx context.Context, participantId [32]byte, 
 		return false, nil
 	})
 
-	err = errors.Wrapf(err, "failed to join request. participantId:%v, requestHash:%v", participantIdHex, requestHashHex)
+	err = errors.Wrapf(err, "failed to join request. partiId:%v, reqHash:%v", partiIdHex, reqHashHex)
 	if err != nil {
 		return nil, rcpt, errors.WithStack(err)
 	}
@@ -89,6 +89,46 @@ func (t *MyTransactor) JoinRequest(ctx context.Context, participantId [32]byte, 
 	return tx, rcpt, nil
 }
 
-func (t *MyTransactor) ReportGeneratedKey(ctx context.Context, participantId [32]byte, generatedPublicKey []byte) (*types.Transaction, error) {
-	return t.boundTransactor.Transact(t.Auth, string(MethodReportGeneratedKey), participantId, generatedPublicKey)
+func (t *MyTransactor) ReportGeneratedKey(ctx context.Context, participantId [32]byte, generatedPublicKey []byte) (*types.Transaction, *types.Receipt, error) {
+	var tx *types.Transaction
+	err := backoff.RetryRetryFnForever(ctx, func() (retry bool, err error) {
+		tx, err = t.boundTransactor.Transact(t.Auth, string(MethodReportGeneratedKey), participantId, generatedPublicKey)
+		if err != nil {
+			errMsg := err.Error()
+			switch {
+			case strings.Contains(errMsg, "AttemptToReconfirmKey"):
+				return false, errors.WithStack(&ErrTypAttemptToReconfirmKey{Cause: err})
+			}
+			return true, errors.WithStack(err)
+		}
+		return false, nil
+	})
+
+	partiIdHex := bytes.Bytes32ToHex(participantId)
+	genPubKeyHex := bytes.BytesToHex(generatedPublicKey)
+	err = errors.Wrapf(err, "failed to report generated public key. partiId:%v, genPubKey:%v", partiIdHex, genPubKeyHex)
+	if err != nil {
+		return tx, nil, err
+	}
+
+	var rcpt *types.Receipt
+	err = backoff.RetryFnExponential10Times(ctx, time.Second, time.Second*10, func() (bool, error) {
+		rcpt, err = t.Receipter.TransactionReceipt(ctx, tx.Hash())
+		if err != nil {
+			return true, errors.WithStack(err)
+		}
+
+		if rcpt.Status != 1 {
+			return true, errors.WithStack(&ErrTypTransactionFailed{})
+		}
+
+		return false, nil
+	})
+
+	err = errors.Wrapf(err, "failed to report generated public key. partiId:%v, genPubKey:%v", partiIdHex, genPubKeyHex)
+	if err != nil {
+		return nil, rcpt, errors.WithStack(err)
+	}
+
+	return tx, rcpt, nil
 }
