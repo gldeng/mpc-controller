a8a0432081661e516713fb68749f5fb19d74ec34
Zealy-Rockx <jigao@rockx.com>
2022-08-06 18:39:51 +0800

    Comment out storage.

--- events/storage.go
+++ events/storage.go
@@ -1,50 +1,51 @@
 package events
 
-// ---------------------------------------------------------------------------------------------------------------------
-// Events concerning local storage
-
-type GroupInfoStored struct {
-	Key string
-	Val GroupInfo
-}
-
-type ParticipantInfoStored struct {
-	Key string
-	Val ParticipantInfo
-}
-
-type GeneratedPubKeyInfoStored struct {
-	Key string
-	Val GeneratedPubKeyInfo
-}
-
-// Info types
-
-// Prefixes
-const (
-	PrefixGroupInfo           = "groupInfo"
-	PrefixParticipantInfo     = "partyInfo"
-	PrefixGeneratedPubKeyInfo = "genPubKeyInfo"
-)
-
-// GroupInfo stored key format: PrefixGroupInfo-GroupIdHex
-type GroupInfo struct {
-	GroupIdHex     string
-	PartPubKeyHexs []string
-	Threshold      uint64
-}
-
-// ParticipantInfo stored key format: PrefixParticipantInfo-PubKeyHashHex-GroupIdHex
-type ParticipantInfo struct {
-	PubKeyHashHex string
-	PubKeyHex     string
-	GroupIdHex    string
-	Index         uint64
-}
-
-// GeneratedPubKeyInfo stored key format: PrefixGeneratedPubKeyInfo-GenPubKeyHashHex
-type GeneratedPubKeyInfo struct {
-	GenPubKeyHashHex       string
-	CompressedGenPubKeyHex string
-	GroupIdHex             string
-}
+//
+//// ---------------------------------------------------------------------------------------------------------------------
+//// Events concerning local storage
+//
+//type GroupInfoStored struct {
+//	Key string
+//	Val GroupInfo
+//}
+//
+//type ParticipantInfoStored struct {
+//	Key string
+//	Val ParticipantInfo
+//}
+//
+//type GeneratedPubKeyInfoStored struct {
+//	Key string
+//	Val GeneratedPubKeyInfo
+//}
+//
+//// Info types
+//
+//// Prefixes
+//const (
+//	PrefixGroupInfo           = "groupInfo"
+//	PrefixParticipantInfo     = "partyInfo"
+//	PrefixGeneratedPubKeyInfo = "genPubKeyInfo"
+//)
+//
+//// GroupInfo stored key format: PrefixGroupInfo-ID
+//type GroupInfo struct {
+//	GroupIdHex     string
+//	PartPubKeyHexs []string
+//	Threshold      uint64
+//}
+//
+//// ParticipantInfo stored key format: PrefixParticipantInfo-PubKeyHashHex-ID
+//type ParticipantInfo struct {
+//	PubKeyHashHex string
+//	PubKeyHex     string
+//	GroupIdHex    string
+//	Index         uint64
+//}
+//
+//// GeneratedPubKeyInfo stored key format: PrefixGeneratedPubKeyInfo-GenPubKeyHashHex
+//type GeneratedPubKeyInfo struct {
+//	GenPubKeyHashHex       string
+//	CompressedGenPubKeyHex string
+//	GroupIdHex             string
+//}
