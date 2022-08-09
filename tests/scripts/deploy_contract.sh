70f3a7e722742aae4b5b29e714e429ab778cb169
Zealy-Rockx <jigao@rockx.com>
2022-08-09 17:24:24 +0800

    Initialize MpcManager.

--- tests/scripts/deploy_contract.sh
+++ tests/scripts/deploy_contract.sh
@@ -15,14 +15,7 @@ LAST_WD=$(pwd)
 
 echo "Start deploying smart contracts"
 
-#read LAST_TEST_WD < $HOME/mpctest/testwd_last
-#
-#cd $LAST_TEST_WD/contracts
-
 cd $HOME/mpctest/contracts/
-
-#rm -rf ./out
-#rm -rf ./cache
 
 # Deploy MpcManager contract
 MPC_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK MpcManager | grep -i "deployed" | cut -d " " -f 3)
@@ -30,14 +23,8 @@ MPC_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROL
 # Deploy AvaLido contract
 AVALIDO=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK AvaLido --constructor-args  $MPC_MANAGER | grep -i "deployed" | cut -d " " -f 3)
 
-# set AvaLido address for MpcManager
-cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $MPC_MANAGER "setAvaLidoAddress(address)" $AVALIDO > /dev/null
-
-# set address to receive principal
-cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $MPC_MANAGER "setReceivePrincipalAddr(address)" $RECEIVE_PRINCIPAL_ADDR > /dev/null
-
-# set address to receive rewards
-cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $MPC_MANAGER "setReceiveRewardAddr(address)" $RECEIVE_REWARD_ADDR > /dev/null
+# Initialize MpcManager contract
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $MPC_MANAGER "initialize(address,address,address,address)" $ROLE_DEFAULT_ADMIN $AVALIDO $RECEIVE_PRINCIPAL_ADDR $RECEIVE_REWARD_ADDR > /dev/null
 
 mkdir -p addresses
 echo -n $MPC_MANAGER > addresses/MPC_MANAGER_ADDRESS
