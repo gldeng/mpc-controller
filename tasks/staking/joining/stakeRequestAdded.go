5cda466492ae24b133cac7be05dd28ceb8dc39cb
Zealy-Rockx <jigao@rockx.com>
2022-08-10 17:14:19 +0800

    Rename reqHash.

--- tasks/staking/joining/stakeRequestAdded.go
+++ tasks/staking/joining/stakeRequestAdded.go
@@ -103,7 +103,7 @@ func (eh *StakeRequestAdded) joinRequest(ctx context.Context) {
 			eh.Logger.WarnOnTrue(float64(len(eh.stakeRequestAddedChan)) > float64(cap(eh.stakeRequestAddedChan))*0.8, "Too many stake request PENDED to join",
 				[]logger.Field{{"pendedStakeReqs", len(eh.stakeRequestAddedChan)}}...)
 			eh.Logger.Debug("Joined stake request", []logger.Field{
-				{"reqNo", evt.RequestNumber}, {"txHash", txHash}}...)
+				{"reqNo", evt.RequestNumber}, {"reqHash", txHash}}...)
 		}
 	}
 }
