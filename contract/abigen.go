2723f3e1fd2d7d9221ad1e1e6dc316f68fc90b6e
Zealy-Rockx <jigao@rockx.com>
2022-08-15 18:26:39 +0800

    Add Oracle contract.

--- contract/abigen.go
+++ contract/abigen.go
@@ -1,6 +1,7 @@
 //go:generate abigen --sol src/AvaLidoMock.sol --pkg contract --out AvaLidoMock.go --type AvaLidoMock
 //go:generate abigen --sol src/MpcManager.sol --pkg contract --out MpcManager.go --type MpcManager
 //go:generate abigen --sol src/OracleManager.sol --pkg contract --out OracleManager.go --type OracleManager
+//go:generate abigen --sol src/Oracle.sol --pkg contract --out Oracle.go --type Oracle
 
 package contract
 
