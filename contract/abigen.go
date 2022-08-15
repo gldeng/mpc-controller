fee281f30a27ec702bdb726782bcb34b97977776
Zealy-Rockx <jigao@rockx.com>
2022-08-15 18:44:57 +0800

    Clean up bindings.

--- contract/abigen.go
+++ contract/abigen.go
@@ -1,9 +1,9 @@
-//go:generate abigen --sol src/AvaLidoMock.sol --pkg contract --out AvaLidoMock.go --type AvaLidoMock
-//go:generate abigen --sol src/AvaLido.sol --pkg contract --out AvaLido.go --type AvaLido
+////go:generate abigen --sol src/AvaLidoMock.sol --pkg contract --out AvaLidoMock.go --type AvaLidoMock
+////go:generate abigen --sol src/AvaLido.sol --pkg contract --out AvaLido.go --type AvaLido
 //go:generate abigen --sol src/MpcManager.sol --pkg contract --out MpcManager.go --type MpcManager
-//go:generate abigen --sol src/OracleManager.sol --pkg contract --out OracleManager.go --type OracleManager
-//go:generate abigen --sol src/Oracle.sol --pkg contract --out Oracle.go --type Oracle
-//go:generate abigen --sol src/ValidatorSelector.sol --pkg contract --out ValidatorSelector.go --type ValidatorSelector
+////go:generate abigen --sol src/OracleManager.sol --pkg contract --out OracleManager.go --type OracleManager
+////go:generate abigen --sol src/Oracle.sol --pkg contract --out Oracle.go --type Oracle
+////go:generate abigen --sol src/ValidatorSelector.sol --pkg contract --out ValidatorSelector.go --type ValidatorSelector
 
 package contract
 
