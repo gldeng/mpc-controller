a7d686e3d5ce56c8ca1b7bd35bffbede3d779478
Zealy-Rockx <jigao@rockx.com>
2022-08-16 10:28:41 +0800

    Initialize contracts.

--- tests/scripts/deploy_contract.sh
+++ tests/scripts/deploy_contract.sh
@@ -4,8 +4,17 @@
 ROLE_DEFAULT_ADMIN="0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
 ROLE_DEFAULT_ADMIN_PK="56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
 
+# Address constants
+LIDO_FEE_ADDRESS="0x2000000000000000000000000000000000000001"
+AUTHOR_FEE_ADDRESS="0x2000000000000000000000000000000000000002"
+
+# Oracle admin
+ORACLE_ADMIN=$ROLE_DEFAULT_ADMIN
+# Oracle allow list
+ORACLE_ALLOW_LIST=("0x03C1196617387899390d3a98fdBdfD407121BB67" "0x6C58f6E7DB68D9F75F2E417aCbB67e7Dd4e413bf" "0xa7bB9405eAF98f36e2683Ba7F36828e260BD0018" "0xE339767906891bEE026285803DA8d8F2f346842C" "0x0309a747a34befD1625b5dcae0B00625FAa30460")
+
 # Pause manager address
-ROLE_PAUSE_MANAGER="0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
+ROLE_PAUSE_MANAGER=$ROLE_DEFAULT_ADMIN
 
 # Addresses to receive principal and rewards after staking period ended
 RECEIVE_PRINCIPAL_ADDR="0xd94fc5fd8812dde061f420d4146bc88e03b6787c"
@@ -25,12 +34,18 @@ VALIDATOR_HELPERS=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-ke
 
 echo "Deploying OracleManager smart contract"
 ORACLE_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK OracleManager --libraries src/Types.sol:ValidatorHelpers:$VALIDATOR_HELPERS  | grep -i "deployed" | cut -d " " -f 3)
+echo "Initializing OracleManager contract"
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $ORACLE_MANAGER "initialize(address,address[])" $ORACLE_ADMIN $ORACLE_ALLOW_LIST > /dev/null
 
 echo "Deploying Oracle smart contract"
 ORACLE=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK Oracle | grep -i "deployed" | cut -d " " -f 3)
+echo "Initializing Oracle contract"
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $ORACLE "initialize(address,address)" $ORACLE_ADMIN $ORACLE_MANAGER > /dev/null
 
 echo "Deploying ValidatorSelector smart contract"
 VALIDATOR_SELECTOR=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK ValidatorSelector --libraries src/Types.sol:ValidatorHelpers:$VALIDATOR_HELPERS  | grep -i "deployed" | cut -d " " -f 3)
+echo "Initializing ValidatorSelector contract"
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $VALIDATOR_SELECTOR "initialize(address)" $ORACLE > /dev/null
 
 echo "Deploying ParticipantIdHelpers smart contract"
 PARTICIPANT_ID_HELPERS=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK ParticipantIdHelpers | grep -i "deployed" | cut -d " " -f 3)
@@ -45,7 +60,11 @@ echo "Deploying MpcManager smart contract"
 MPC_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK MpcManager --libraries src/MpcManager.sol:ParticipantIdHelpers:$PARTICIPANT_ID_HELPERS --libraries src/MpcManager.sol:ConfirmationHelpers:$CONFIRMATION_HELPERS --libraries src/MpcManager.sol:KeygenStatusHelpers:$KEYGEN_STATUS_HELPERS  | grep -i "deployed" | cut -d " " -f 3)
 
 echo "Deploying AvaLido smart contract"
-AVALIDO=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK AvaLido --constructor-args  $MPC_MANAGER | grep -i "deployed" | cut -d " " -f 3)
+#AVALIDO=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK AvaLido --constructor-args  $MPC_MANAGER | grep -i "deployed" | cut -d " " -f 3)
+AVALIDO=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK AvaLido | grep -i "deployed" | cut -d " " -f 3)
+echo "Initializing AvaLido contract"
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $AVALIDO "initialize(address,address,address,address)" $LIDO_FEE_ADDRESS $AUTHOR_FEE_ADDRESS $VALIDATOR_SELECTOR $MPC_MANAGER > /dev/null
+
 
 echo "Initializing MpcManager contract"
 cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $MPC_MANAGER "initialize(address,address,address,address,address)" $ROLE_DEFAULT_ADMIN $ROLE_PAUSE_MANAGER $AVALIDO $RECEIVE_PRINCIPAL_ADDR $RECEIVE_REWARD_ADDR > /dev/null
