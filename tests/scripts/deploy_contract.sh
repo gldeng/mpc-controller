f2327b6535f2f7dafa121a6ee438e9c629fdb8eb
Zealy-Rockx <jigao@rockx.com>
2022-08-16 10:02:12 +0800

    Add more contracts.

--- tests/scripts/deploy_contract.sh
+++ tests/scripts/deploy_contract.sh
@@ -18,27 +18,35 @@ LAST_WD=$(pwd)
 
 cd $HOME/mpctest/contracts/
 
-# Deploy ParticipantIdHelpers contract
-echo "Start deploying ParticipantIdHelpers smart contract"
+echo "Start deploying contracts..."
+
+echo "Deploying ValidatorHelpers smart contract"
+VALIDATOR_HELPERS=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK ValidatorHelpers | grep -i "deployed" | cut -d " " -f 3)
+
+echo "Deploying OracleManager smart contract"
+ORACLE_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK OracleManager --libraries src/Types.sol:ValidatorHelpers:$VALIDATOR_HELPERS  | grep -i "deployed" | cut -d " " -f 3)
+
+echo "Deploying Oracle smart contract"
+ORACLE=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK Oracle | grep -i "deployed" | cut -d " " -f 3)
+
+echo "Deploying ValidatorSelector smart contract"
+VALIDATOR_SELECTOR=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK ValidatorSelector --libraries src/Types.sol:ValidatorHelpers:$VALIDATOR_HELPERS  | grep -i "deployed" | cut -d " " -f 3)
+
+echo "Deploying ParticipantIdHelpers smart contract"
 PARTICIPANT_ID_HELPERS=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK ParticipantIdHelpers | grep -i "deployed" | cut -d " " -f 3)
 
-# Deploy ConfirmationHelpers contract
-echo "Start deploying ConfirmationHelpers smart contract"
+echo "Deploying ConfirmationHelpers smart contract"
 CONFIRMATION_HELPERS=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK ConfirmationHelpers | grep -i "deployed" | cut -d " " -f 3)
 
-# Deploy KeygenStatusHelpers contract
-echo "Start deploying KeygenStatusHelpers smart contract"
+echo "Deploying KeygenStatusHelpers smart contract"
 KEYGEN_STATUS_HELPERS=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK KeygenStatusHelpers | grep -i "deployed" | cut -d " " -f 3)
 
-# Deploy MpcManager contract
-echo "Start deploying MpcManager smart contract"
+echo "Deploying MpcManager smart contract"
 MPC_MANAGER=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK MpcManager --libraries src/MpcManager.sol:ParticipantIdHelpers:$PARTICIPANT_ID_HELPERS --libraries src/MpcManager.sol:ConfirmationHelpers:$CONFIRMATION_HELPERS --libraries src/MpcManager.sol:KeygenStatusHelpers:$KEYGEN_STATUS_HELPERS  | grep -i "deployed" | cut -d " " -f 3)
 
-# Deploy AvaLido contract
-echo "Start deploying AvaLido smart contract"
+echo "Deploying AvaLido smart contract"
 AVALIDO=$(forge create --force --rpc-url $C_CHAIN_RPC_URL --private-key $ROLE_DEFAULT_ADMIN_PK AvaLido --constructor-args  $MPC_MANAGER | grep -i "deployed" | cut -d " " -f 3)
 
-# Initialize MpcManager contract
 echo "Initializing MpcManager contract"
 cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_DEFAULT_ADMIN --private-key $ROLE_DEFAULT_ADMIN_PK $MPC_MANAGER "initialize(address,address,address,address,address)" $ROLE_DEFAULT_ADMIN $ROLE_PAUSE_MANAGER $AVALIDO $RECEIVE_PRINCIPAL_ADDR $RECEIVE_REWARD_ADDR > /dev/null
 
@@ -46,7 +54,16 @@ mkdir -p addresses
 echo -n $MPC_MANAGER > addresses/MPC_MANAGER_ADDRESS
 echo -n $AVALIDO > addresses/AVALIDO_ADDRESS
 
-echo "MpcManager address: $MPC_MANAGER"
-echo "AvaLido address: $AVALIDO"
+echo "Deployed contract addresses..."
+
+echo "ValidatorHelpers address:     $VALIDATOR_HELPERS"
+echo "OracleManager address:        $ORACLE_MANAGER"
+echo "Oracle address:               $ORACLE"
+echo "ValidatorSelector address:    $VALIDATOR_SELECTOR"
+echo "ParticipantIdHelpers address: $PARTICIPANT_ID_HELPERS"
+echo "ConfirmationHelpers address:  $CONFIRMATION_HELPERS"
+echo "KeygenStatusHelpers address:  $KEYGEN_STATUS_HELPERS"
+echo "MpcManager address:           $MPC_MANAGER"
+echo "AvaLido address:              $AVALIDO"
 
 cd $LAST_WD
