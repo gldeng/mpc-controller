6c46ae142907896777ce1b6bb1d494497e9f0040
Zealy-Rockx <jigao@rockx.com>
2022-08-16 10:36:18 +0800

    Add Treasury.sol

--- contract/abigen.go
+++ contract/abigen.go
@@ -1,5 +1,6 @@
 ////go:generate abigen --sol src/AvaLidoMock.sol --pkg contract --out AvaLidoMock.go --type AvaLidoMock
 ////go:generate abigen --sol src/AvaLido.sol --pkg contract --out AvaLido.go --type AvaLido
+//go:generate abigen --sol src/Treasury.sol --pkg contract --out Treasury.go --type Treasury
 //go:generate abigen --sol src/MpcManager.sol --pkg contract --out MpcManager.go --type MpcManager
 ////go:generate abigen --sol src/OracleManager.sol --pkg contract --out OracleManager.go --type OracleManager
 ////go:generate abigen --sol src/Oracle.sol --pkg contract --out Oracle.go --type Oracle
