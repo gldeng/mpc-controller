2ef4e150de2821a32a71ddae449ad9cbd34ea233
Zealy-Rockx <jigao@rockx.com>
2022-08-28 21:33:11 +0800

    Set max protocol controlled AVAX.

--- tests/scripts/deploy_contract.sh
+++ tests/scripts/deploy_contract.sh
@@ -52,10 +52,13 @@ cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_ORACLE_ADMIN --private-key $RO
 # Set epoch duration for Oracle
 cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_ORACLE_ADMIN --private-key $ROLE_ORACLE_ADMIN_PK --gas-limit 900000 $ORACLE "setEpochDuration(uint256)" 17 > /dev/null
 
+# Set max protocol controlled AVAX for AvaLido
+cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_PROTOCOL_MANAGER --private-key $ROLE_PROTOCOL_MANAGER_PK --gas-limit 900000 $AVALIDO "setMaxProtocolControlledAVAX(uint256)" 50000000 > /dev/null
+
 # Set stake period for AvaLido
 cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_PROTOCOL_MANAGER --private-key $ROLE_PROTOCOL_MANAGER_PK --gas-limit 900000 $AVALIDO "setStakePeriod(uint256)" 600 > /dev/null
 
-# Set P-Chain export buffer
+# Set P-Chain export buffer for AvaLido
 cast send --rpc-url $C_CHAIN_RPC_URL --from $ROLE_PROTOCOL_MANAGER --private-key $ROLE_PROTOCOL_MANAGER_PK --gas-limit 900000 $AVALIDO "setPChainExportBuffer(uint256)" 300 > /dev/null
 
 cd $LAST_WD
