a15ca2d9d4840d044f74c2b428375c33a27237b9
Zealy-Rockx <jigao@rockx.com>
2022-10-11 18:00:23 +0800

    Add atomicTxTaskAdapter

---
+++ tasks_/atomicTxTask/atomicTxTaskAdapter.go
@@ -0,0 +1,32 @@
+package atomicTxTask
+
+import (
+	"github.com/avalido/mpc-controller/events"
+	staking "github.com/avalido/mpc-controller/tasks_"
+	kbcevents "github.com/kubecost/events"
+)
+
+type AtomicTxTaskAdapter struct {
+	ReqStartedEvtDispatcher kbcevents.Dispatcher[*events.RequestStarted]
+	StakeTaskCreator        *staking.StakeTaskCreator
+}
+
+//func (a *AtomicTxTaskAdapter) Init() {
+//	a.ReqStartedEvtDispatcher.AddFilteredEventHandler(reqStartedEvtHandler, reqStartedEvtFilter)
+//}
+
+//func (a *AtomicTxTaskAdapter) re
+//
+//
+//var reqStartedEvtHandler = func(evt *events.RequestStarted) {
+//	stakeReq := evt.JoinedReq.Args.(*storage.StakeRequest)
+//	stakeTask, _ := .createStakeTask(stakeReq, *evt.ReqHash)
+//	// todo: logic...
+//}
+//
+//var reqStartedEvtFilter = func(evt *events.RequestStarted) bool {
+//	return evt.TaskType == storage.TaskTypStake
+//}
+
+// decouple
+// async, sync?
