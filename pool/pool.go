95a31bbb3b5be017415988b76371403b301e4db4
gldeng <realgldeng@gmail.com>
2022-10-21 11:03:21 +0800

    Add task types

--- pool/pool.go
+++ pool/pool.go
@@ -1,6 +1,43 @@
 package pool
 
-type WorkerPool interface {
-	Submit(task func())
-	StopAndWait()
+import (
+	"github.com/alitto/pond"
+	"github.com/enriquebris/goconcurrentqueue"
+)
+
+type ExtendedWorkerPool struct {
+	inner     *pond.WorkerPool
+	resources *goconcurrentqueue.FIFO
+}
+
+func New(size int) (*ExtendedWorkerPool, error) {
+	inner := pond.New(size, 1024)
+	return &ExtendedWorkerPool{
+		inner: inner,
+	}, nil
+}
+
+func (e *ExtendedWorkerPool) Start() error {
+	// Do nothing
+	return nil
+}
+
+func (e *ExtendedWorkerPool) Close() error {
+	e.inner.StopAndWait()
+	return nil
+}
+
+func (e *ExtendedWorkerPool) Submit(task Task) error {
+	taskWrapper := func() {
+		res, _ := e.resources.Dequeue()        // TODO: Handle error
+		next, _ := task.Next(res.(*Resources)) // TODO: Handle error
+		e.resources.Enqueue(res)
+		if next != nil {
+			for _, t := range next {
+				e.Submit(t) // Task needs to continue with itself or succeeding tasks
+			}
+		}
+	}
+	e.inner.Submit(taskWrapper)
+	return nil
 }
---
+++ pool/types.go
@@ -0,0 +1,29 @@
+package pool
+
+import (
+	"github.com/avalido/mpc-controller/chain"
+	"github.com/avalido/mpc-controller/chain/txissuer"
+	"github.com/avalido/mpc-controller/core"
+	"github.com/avalido/mpc-controller/logger"
+	"github.com/avalido/mpc-controller/utils/noncer"
+)
+
+// TODO: Deprecate
+type WorkerPool interface {
+	Submit(task func())
+	StopAndWait()
+}
+
+type Task interface {
+	Next(resources *Resources) ([]Task, error)
+}
+
+type Resources struct { // TODO: Convert it to TaskApi interface instead of directly giving the underlying resources
+	Logger logger.Logger
+
+	NonceGiver noncer.Noncer
+	Network    chain.NetworkContext
+
+	MpcClient core.MpcClient
+	TxIssuer  txissuer.TxIssuer
+}
