70a1aa0a869c6427ca791abad5a1189587c0eba9
gldeng <realgldeng@gmail.com>
2022-10-22 09:31:44 +0800

    Clean up

--- tasks/c2p/task_export.go
+++ tasks/c2p/task_export.go
@@ -27,15 +27,6 @@ type ExportFromCChain struct {
 	TxCred      *secp256k1fx.Credential
 	TxID        *ids.ID
 	SignRequest *core.SignRequest
-	//Txs             *Txs
-	//SignReqs        []*core.SignRequest
-	//ExportIssueTx   *txissuer.Tx
-	//ExportTxStatus  pool.Status
-	//ExportTxSignRes *core.Result
-	//
-	//ImportIssueTx   *txissuer.Tx
-	//ImportTxStatus  pool.Status
-	//ImportTxSignRes *core.Result
 }
 
 func (t *ExportFromCChain) IsDone() bool {
@@ -54,8 +45,6 @@ func NewExportFromCChain(id string, amount big.Int, quorum QuorumInfo) (*ExportF
 		SignRequest: nil,
 	}, nil
 }
-
-// SignRequestSent
 
 func (t *ExportFromCChain) Next(ctx pool.TaskContext) ([]pool.Task, error) {
 	self := []pool.Task{t}
@@ -106,41 +95,6 @@ func (t *ExportFromCChain) SignedTx() (*evm.Tx, error) {
 	return PackSignedExportTx(t.Tx, t.TxCred)
 }
 
-//func (t *ExportFromCChain) buildTask(ctx pool.TaskContext) error {
-//	txs, err := t.buildTxs(ctx)
-//	if err != nil {
-//		return errors.Wrapf(err, "failed to build txs")
-//	}
-//
-//	signReq, err := t.buildExportTxSignReq(txs)
-//	if err != nil {
-//		return errors.Wrapf(err, "failed to build ExportTx signing request")
-//	}
-//
-//	t.Txs = txs
-//	t.SignReqs = make([]*core.SignRequest, 2)
-//	t.SignReqs[0] = signReq
-//	return nil
-//}
-
-//func (t *ExportFromCChain) buildExportTxSignReq(txs *Txs) (*core.SignRequest, error) {
-//	exportTxHash, err := txs.ExportTxHash()
-//	if err != nil {
-//		return nil, errors.Wrapf(err, "failed to get ExportTx hash")
-//	}
-//
-//	return t.buildSignReq(t.Id+"/export", exportTxHash)
-//}
-//
-//func (t *ExportFromCChain) buildImportTxSignReq(txs *Txs) (*core.SignRequest, error) {
-//	importTxHash, err := txs.ImportTxHash()
-//	if err != nil {
-//		return nil, errors.Wrapf(err, "failed to get ImportTx hash")
-//	}
-//
-//	return t.buildSignReq(t.Id+"/import", importTxHash)
-//}
-
 func (t *ExportFromCChain) buildSignReq(id string, hash []byte) (*core.SignRequest, error) {
 	var participantPks []string
 	for _, pk := range t.Quorum.ParticipantPubKeys {
@@ -153,30 +107,3 @@ func (t *ExportFromCChain) buildSignReq(id string, hash []byte) (*core.SignReque
 		Hash:                   bytes.BytesToHex(hash),
 	}, nil
 }
-
-//func (t *ExportFromCChain) buildTxs(ctx pool.TaskContext) (*Txs, error) {
-//
-//	nAVAXAmount := new(big.Int).Div(&t.Amount, big.NewInt(1_000_000_000))
-//	if !nAVAXAmount.IsUint64() {
-//		return nil, errors.New(ErrMsgInvalidUint64)
-//	}
-//
-//	cChainAddr, _ := (storage.PubKey(t.Quorum.PubKey)).CChainAddress()
-//	pChainAddr, _ := (storage.PubKey(t.Quorum.PubKey)).PChainAddress()
-//	nonce, _ := ctx.NonceAt(cChainAddr)
-//
-//	st := Txs{
-//		Nonce:         nonce,
-//		DelegateAmt:   nAVAXAmount.Uint64(),
-//		CChainAddress: cChainAddr,
-//		PChainAddress: pChainAddr,
-//
-//		BaseFeeGwei: cchain.BaseFeeGwei,
-//		NetworkID:   ctx.GetNetwork().NetworkID(),
-//		CChainID:    ctx.GetNetwork().CChainID(),
-//		Asset:       ctx.GetNetwork().Asset(),
-//		ImportFee:   ctx.GetNetwork().ImportFee(),
-//	}
-//
-//	return &st, nil
-//}
