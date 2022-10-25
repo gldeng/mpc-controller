4746ae24953e70f8d1f6f9e811ebb868a6ee7e08
gldeng <realgldeng@gmail.com>
2022-10-25 10:28:00 +0800

    Add SimulatingMpcClient

---
+++ core/mpcclient_test.go
@@ -0,0 +1,43 @@
+package core
+
+import (
+	"context"
+	"encoding/hex"
+	ethCrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestSimulatingMpcClient(t *testing.T) {
+	reqIDStr := "123"
+	privKeyStr := "56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
+	hashStr := "1111111111111111111111111111111111111111111111111111111111111111"
+	hash, _ := hex.DecodeString(hashStr)
+	privateKey, _ := ethCrypto.HexToECDSA(privKeyStr)
+	client, _ := NewSimulatingMpcClient(privKeyStr)
+	client.Keygen(context.Background(), &KeygenRequest{
+		ReqID:                  "kg",
+		CompressedPartiPubKeys: []string{},
+		Threshold:              0,
+	})
+	res, _ := client.Result(context.Background(), "kg")
+
+	pkBytes, _ := hex.DecodeString(res.Result)
+	pk, _ := ethCrypto.DecompressPubkey(pkBytes)
+
+	require.Equal(t, privateKey.PublicKey, *pk)
+	client.Sign(context.Background(), &SignRequest{
+		ReqID:                  reqIDStr,
+		Hash:                   hashStr,
+		CompressedGenPubKeyHex: "",
+		CompressedPartiPubKeys: []string{},
+	})
+	pubKey := ethCrypto.FromECDSAPub(&privateKey.PublicKey)
+
+	res, _ = client.Result(context.Background(), "123")
+	sig, _ := hex.DecodeString(res.Result)
+
+	pubKeyRecovered, err := ethCrypto.Ecrecover(hash, sig)
+	require.NoError(t, err)
+	require.Equal(t, pubKey, pubKeyRecovered)
+}
