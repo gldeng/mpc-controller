70d0a1ed841860359808dc65e9758b6f9a4ebed3
Zealy-Rockx <jigao@rockx.com>
2022-11-08 17:31:12 +0800

    Simplify bytes utils

--- utils/bytes/bytes2hex.go
+++ utils/bytes/bytes2hex.go
@@ -4,30 +4,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-func BytesToHexArr(bytesArr [][]byte) []string {
-	var res []string
-	for _, bytes := range bytesArr {
-		res = append(res, BytesToHex(bytes))
-	}
-	return res
-}
-
-func Bytes32ToHexArr(bytesArr [][32]byte) []string {
-	var res []string
-	for _, bytes := range bytesArr {
-		res = append(res, Bytes32ToHex(bytes))
-	}
-	return res
-}
-
-func Bytes65ToHexArr(bytesArr [][65]byte) []string {
-	var res []string
-	for _, bytes := range bytesArr {
-		res = append(res, Bytes65ToHex(bytes))
-	}
-	return res
-}
-
 func BytesToHex(bytes []byte) string {
 	return common.Bytes2Hex(bytes)
 }
