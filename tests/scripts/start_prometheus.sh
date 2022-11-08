229b936f0f5e56ab623fc3035e7cc0ac75ceef76
Zealy-Rockx <jigao@rockx.com>
2022-11-08 17:16:23 +0800

    Delete scripts

--- tests/scripts/start_prometheus.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-promImage=$(docker image ls | grep prom/prometheus)
-if [ -z "$promImage" ]; then
-  echo "Pulling prom/prometheus"
-  docker pull prom/prometheus
-fi
-
-docker run --network="host"  -p 9090:9090 -v $(pwd)/tests/configs/prom:/etc/prometheus prom/prometheus > /dev/null
