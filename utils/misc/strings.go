0bc35eb79fb3c2936463bc483b34cea4bd964d74
Zealy-Rockx <jigao@rockx.com>
2022-11-15 15:35:54 +0800

    Update text extracting util

--- utils/misc/strings.go
+++ utils/misc/strings.go
@@ -3,12 +3,43 @@ package misc
 import "strings"
 
 func ExtractColumIntoString(s string, sep string, col int) string {
-	lines := strings.Split(s, "\n")
+	cols := ExtractColum(s, sep, col)
 	var r string
-	for _, line := range lines {
-		fields := strings.Split(line, sep)
-		r += fields[col] + " "
+	for _, field := range cols {
+		r += field + " "
 	}
-	r = strings.TrimRight(r, " ")
-	return r
+	return strings.TrimSpace(r)
+}
+
+func ExtractColum(s string, sep string, col int) []string {
+	allFields := SplitText(s, sep)
+	var fields_ []string
+	for _, fields := range allFields {
+		if len(fields) <= col {
+			continue
+		}
+		fields_ = append(fields_, fields[col])
+	}
+	return fields_
+}
+
+func SplitText(s string, sep string) [][]string {
+	var allFields [][]string
+	lines := strings.Split(s, "\n")
+	for _, line := range lines {
+		var lineFields []string
+		fields := strings.Split(line, sep)
+		for _, field := range fields {
+			field := strings.TrimSpace(field)
+			if field == "" {
+				continue
+			}
+			lineFields = append(lineFields, field)
+		}
+		if len(lineFields) == 0 {
+			continue
+		}
+		allFields = append(allFields, lineFields)
+	}
+	return allFields
 }
--- utils/misc/strings_test.go
+++ utils/misc/strings_test.go
@@ -1,29 +1,29 @@
 package misc
 
 import (
-	"github.com/stretchr/testify/require"
+	"fmt"
 	"testing"
 )
 
-func TestColum(t *testing.T) {
-	s := `29abd4c6e599   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-7_controller_1
-cbf9a4610131   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-6_controller_1
-e01f9247f5a4   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-5_controller_1
-afa3c87db82a   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-4_controller_1
-7ac33878b1db   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-3_controller_1
-caaa9197fb13   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-2_controller_1
-b68d8590dc60   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 3 minutes ago                                                mpc-1_controller_1
-b2b61df5934f   avalido/oracle                                         "/app/oracle --rpc-u…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc-1_oracle_1
-7b5fa537f004   avalido/mpc-server                                     "/app/mpc-server -s …"   20 hours ago   Created                     0.0.0.0:8001->8001/tcp, :::8001->8001/tcp   mpc-1_server_1
-969cc1c87db3   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller2
-1bd9b2c789f7   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller1
-7f0aefac00fc   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller7
-b1ccbac8a8b7   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller3
-056401c90dbe   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller4
-38ce8224e957   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller6
-e54b47f0c6fe   avalido/mpc-controller                                 "/app/mpc-controller…"   20 hours ago   Exited (0) 20 hours ago                                                 mpc_controller5
+const text = `avalido/mpc-controller                          latest               1dd8f4ee9838   26 hours ago    44MB
+<none>                                          <none>               63a7a46800a8   27 hours ago    1.21GB
+<none>                                          <none>               6a944206581d   27 hours ago    1.21GB
+vektra/mockery                                  latest               0c365eaaf3e4   2 weeks ago     337MB
 `
-	expected := "29abd4c6e599 cbf9a4610131 e01f9247f5a4 afa3c87db82a 7ac33878b1db caaa9197fb13 b68d8590dc60 b2b61df5934f 7b5fa537f004 969cc1c87db3 1bd9b2c789f7 7f0aefac00fc b1ccbac8a8b7 056401c90dbe 38ce8224e957 e54b47f0c6fe"
-	col := ExtractColumIntoString(s, " ", 0)
-	require.Equal(t, col, expected)
+
+func TestExtractColumIntoString(t *testing.T) {
+	col := ExtractColumIntoString(text, " ", 2)
+	fmt.Println(col)
+}
+
+func TestExtractColum(t *testing.T) {
+	col := ExtractColum(text, " ", 2)
+	fmt.Printf("Len:%v Val:%v", len(col), col)
+}
+
+func TestSplitText(t *testing.T) {
+	lines := SplitText(text, " ")
+	for _, line := range lines {
+		fmt.Printf("Len:%v, Val:%v\n", len(line), line)
+	}
 }
