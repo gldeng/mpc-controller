e4f09080dd48727c82203531ba4b25f80e751d4b
Zealy-Rockx <jigao@rockx.com>
2022-11-30 17:26:44 +0800

    Add FlowId and TaskType.

--- tasks/c2p/task_c2p.go
+++ tasks/c2p/task_c2p.go
@@ -6,7 +6,12 @@ import (
 
 	"github.com/avalido/mpc-controller/core"
 	"github.com/avalido/mpc-controller/core/types"
+	"github.com/avalido/mpc-controller/logger"
 	"github.com/pkg/errors"
+)
+
+const (
+	taskTypeC2P = "c2p"
 )
 
 var (
@@ -14,13 +19,28 @@ var (
 )
 
 type C2P struct {
-	Status Status
-	FlowId string
-	Quorum types.QuorumInfo
+	Status   Status
+	FlowId   string
+	TaskType string
+	Quorum   types.QuorumInfo
 
 	ExportTask      *ExportFromCChain
 	ImportTask      *ImportIntoPChain
 	SubTaskHasError error
+}
+
+func NewC2P(FlowId string, quorum types.QuorumInfo, amount big.Int) (*C2P, error) {
+	exportTask, err := NewExportFromCChain(FlowId, quorum, amount)
+	if err != nil {
+		return nil, err
+	}
+	return &C2P{
+		Status:     StatusInit,
+		FlowId:     FlowId,
+		TaskType:   taskTypeC2P,
+		Quorum:     quorum,
+		ExportTask: exportTask,
+	}, nil
 }
 
 func (t *C2P) GetId() string {
@@ -66,7 +86,7 @@ func (t *C2P) run(ctx core.TaskContext) ([]core.Task, error) {
 		if t.ExportTask.IsDone() {
 			err := t.startImport()
 			if err != nil {
-				ctx.GetLogger().Errorf("Failed to start import, error:%+v", err)
+				t.logError(ctx, "failed to start import task", err)
 			}
 		}
 		return next, err
@@ -77,22 +97,26 @@ func (t *C2P) run(ctx core.TaskContext) ([]core.Task, error) {
 			t.SubTaskHasError = err
 		}
 		if t.ImportTask.IsDone() {
-			ctx.GetLogger().Debugf("%v imported", t.FlowId)
+			t.logDebug(ctx, "import task done")
 		}
 		return next, err
 	}
 	return nil, errors.New("invalid state of composite task")
 }
 
-func NewC2P(FlowId string, quorum types.QuorumInfo, amount big.Int) (*C2P, error) {
-	exportTask, err := NewExportFromCChain(FlowId, quorum, amount)
-	if err != nil {
-		return nil, err
-	}
-	return &C2P{
-		Status:     StatusInit,
-		FlowId:     FlowId,
-		Quorum:     quorum,
-		ExportTask: exportTask,
-	}, nil
+func (t *C2P) logDebug(ctx core.TaskContext, msg string, fields ...logger.Field) {
+	allFields := make([]logger.Field, 0, len(fields)+2)
+	allFields = append(allFields, logger.Field{"flowId", t.FlowId})
+	allFields = append(allFields, logger.Field{"taskType", t.TaskType})
+	allFields = append(allFields, fields...)
+	ctx.GetLogger().Debug(msg, allFields...)
+}
+
+func (t *C2P) logError(ctx core.TaskContext, msg string, err error, fields ...logger.Field) {
+	allFields := make([]logger.Field, 0, len(fields)+3)
+	allFields = append(allFields, logger.Field{"flowId", t.FlowId})
+	allFields = append(allFields, logger.Field{"taskType", t.TaskType})
+	allFields = append(allFields, fields...)
+	allFields = append(allFields, logger.Field{"error", fmt.Sprintf("%+v", err)})
+	ctx.GetLogger().Error(msg, allFields...)
 }
