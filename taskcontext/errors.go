e1d97dc2c442039e54eef7a946024a4f12a149dc
Zealy-Rockx <jigao@rockx.com>
2022-12-09 17:20:37 +0800

    taskcontext: check error cause.

--- taskcontext/errors.go
+++ taskcontext/errors.go
@@ -12,131 +12,125 @@ const (
 // ----------error types ----------
 
 type ErrTypCreateTransactor struct {
-	Msg string
-	Pre error
+	Msg   string
+	Cause error
 }
 
 func (e *ErrTypCreateTransactor) Error() string {
 	if e.Msg == "" {
 		e.Msg = ErrMsgCreateTransactor
 	}
-	return e.Msg + ": " + e.Pre.Error()
-}
-
-func (e *ErrTypCreateTransactor) Cause() error {
-	return e.Pre
+	if e.Cause == nil {
+		return e.Msg
+	}
+	return e.Msg + ": " + e.Cause.Error()
 }
 
 func (e *ErrTypCreateTransactor) Unwrap() error {
-	return e.Pre
+	return e.Cause
 }
 
 // ----------
 
 type ErrTypCallTransactor struct {
-	Msg string
-	Pre error
+	Msg   string
+	Cause error
 }
 
 func (e *ErrTypCallTransactor) Error() string {
 	if e.Msg == "" {
 		e.Msg = ErrMsgCallTransactor
 	}
-	return e.Msg + ": " + e.Pre.Error()
-}
-
-func (e *ErrTypCallTransactor) Cause() error {
-	return e.Pre
+	if e.Cause == nil {
+		return e.Msg
+	}
+	return e.Msg + ": " + e.Cause.Error()
 }
 
 func (e *ErrTypCallTransactor) Unwrap() error {
-	return e.Pre
+	return e.Cause
 }
 
 // ----------
 
 type ErrTypTxAborted struct {
-	Msg string
-	Pre error
+	Msg   string
+	Cause error
 }
 
 func (e *ErrTypTxAborted) Error() string {
 	if e.Msg == "" {
 		e.Msg = ErrMsgTxAborted
 	}
-	return e.Msg + ": " + e.Pre.Error()
-}
-
-func (e *ErrTypTxAborted) Cause() error {
-	return e.Pre
+	if e.Cause == nil {
+		return e.Msg
+	}
+	return e.Msg + ": " + e.Cause.Error()
 }
 
 func (e *ErrTypTxAborted) Unwrap() error {
-	return e.Pre
+	return e.Cause
 }
 
 // ----------
 
 type ErrTypExecutionReverted struct {
-	Msg string
-	Pre error
+	Msg   string
+	Cause error
 }
 
 func (e *ErrTypExecutionReverted) Error() string {
 	if e.Msg == "" {
 		e.Msg = ErrMsgExecutionReverted
 	}
-	return e.Msg + ": " + e.Pre.Error()
-}
-
-func (e *ErrTypExecutionReverted) Cause() error {
-	return e.Pre
+	if e.Cause == nil {
+		return e.Msg
+	}
+	return e.Msg + ": " + e.Cause.Error()
 }
 
 func (e *ErrTypExecutionReverted) Unwrap() error {
-	return e.Pre
+	return e.Cause
 }
 
 // ----------
 
 type ErrTypReceiptNotFound struct {
-	Msg string
-	Pre error
+	Msg   string
+	Cause error
 }
 
 func (e *ErrTypReceiptNotFound) Error() string {
 	if e.Msg == "" {
 		e.Msg = ErrMsgReceiptNotFound
 	}
-	return e.Msg + ": " + e.Pre.Error()
-}
-
-func (e *ErrTypReceiptNotFound) Cause() error {
-	return e.Pre
+	if e.Cause == nil {
+		return e.Msg
+	}
+	return e.Msg + ": " + e.Cause.Error()
 }
 
 func (e *ErrTypReceiptNotFound) Unwrap() error {
-	return e.Pre
+	return e.Cause
 }
 
 // ----------
 
 type ErrTypQueryReceipt struct {
-	Msg string
-	Pre error
+	Msg   string
+	Cause error
 }
 
 func (e *ErrTypQueryReceipt) Error() string {
 	if e.Msg == "" {
 		e.Msg = ErrMsgQueryReceipt
 	}
-	return e.Msg + ": " + e.Pre.Error()
-}
-
-func (e *ErrTypQueryReceipt) Cause() error {
-	return e.Pre
+	if e.Cause == nil {
+		return e.Msg
+	}
+	return e.Msg + ": " + e.Cause.Error()
 }
 
 func (e *ErrTypQueryReceipt) Unwrap() error {
-	return e.Pre
+	return e.Cause
 }
--- taskcontext/task_context.go
+++ taskcontext/task_context.go
@@ -47,9 +47,9 @@ func (t *TaskContextImp) CheckEthTx(txHash common.Hash) (core.TxStatus, error) {
 	rcp, err := t.EthClient.TransactionReceipt(context.Background(), txHash)
 	if err != nil {
 		if strings.Contains(err.Error(), interfaces.NotFound.Error()) {
-			return core.TxStatusUnknown, errors.WithStack(&ErrTypReceiptNotFound{Pre: err})
+			return core.TxStatusUnknown, errors.WithStack(&ErrTypReceiptNotFound{Cause: err})
 		}
-		return core.TxStatusUnknown, errors.WithStack(&ErrTypQueryReceipt{Pre: err})
+		return core.TxStatusUnknown, errors.WithStack(&ErrTypQueryReceipt{Cause: err})
 	}
 
 	if rcp.Status == coreTypes.ReceiptStatusFailed {
@@ -61,16 +61,16 @@ func (t *TaskContextImp) CheckEthTx(txHash common.Hash) (core.TxStatus, error) {
 func (t *TaskContextImp) ReportGeneratedKey(opts *bind.TransactOpts, participantId [32]byte, generatedPublicKey []byte) (*common.Hash, error) {
 	transactor, err := contract.NewMpcManagerTransactor(t.Services.Config.MpcManagerAddress, t.EthClient)
 	if err != nil {
-		return nil, errors.WithStack(&ErrTypCreateTransactor{Pre: err})
+		return nil, errors.WithStack(&ErrTypCreateTransactor{Cause: err})
 	}
 
 	var hash common.Hash
 	tx, err := transactor.ReportGeneratedKey(opts, participantId, generatedPublicKey)
 	if err != nil {
 		if strings.Contains(err.Error(), vm.ErrExecutionReverted.Error()) {
-			return nil, errors.WithStack(&ErrTypExecutionReverted{Pre: err})
+			return nil, errors.WithStack(&ErrTypExecutionReverted{Cause: err})
 		}
-		return nil, errors.WithStack(&ErrTypCallTransactor{Pre: err})
+		return nil, errors.WithStack(&ErrTypCallTransactor{Cause: err})
 	}
 
 	hash = tx.Hash()
@@ -80,16 +80,16 @@ func (t *TaskContextImp) ReportGeneratedKey(opts *bind.TransactOpts, participant
 func (t *TaskContextImp) JoinRequest(opts *bind.TransactOpts, participantId [32]byte, requestHash [32]byte) (*common.Hash, error) {
 	transactor, err := contract.NewMpcManagerTransactor(t.Services.Config.MpcManagerAddress, t.EthClient)
 	if err != nil {
-		return nil, errors.WithStack(&ErrTypCreateTransactor{Pre: err})
+		return nil, errors.WithStack(&ErrTypCreateTransactor{Cause: err})
 	}
 
 	var hash common.Hash
 	tx, err := transactor.JoinRequest(opts, participantId, requestHash)
 	if err != nil {
 		if strings.Contains(err.Error(), vm.ErrExecutionReverted.Error()) {
-			return nil, errors.WithStack(&ErrTypExecutionReverted{Pre: err})
+			return nil, errors.WithStack(&ErrTypExecutionReverted{Cause: err})
 		}
-		return nil, errors.WithStack(&ErrTypCallTransactor{Pre: err})
+		return nil, errors.WithStack(&ErrTypCallTransactor{Cause: err})
 	}
 
 	hash = tx.Hash()
