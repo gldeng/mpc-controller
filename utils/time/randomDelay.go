95272c5b2ee3b360d5868d901766cdc49eb109e1
Zealy-Rockx <jigao@rockx.com>
2022-12-19 14:53:09 +0800

    utils/time: update function signature #164

--- utils/time/randomDelay.go
+++ utils/time/randomDelay.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-func RandomAfter(milliSeconds int64) {
+func RandomDelay(maxMilliSeconds int64) {
 	rand.Seed(time.Now().UnixNano())
-	random := rand.Int63n(milliSeconds)
+	random := rand.Int63n(maxMilliSeconds)
 	<-time.After(time.Millisecond * time.Duration(random))
 }
