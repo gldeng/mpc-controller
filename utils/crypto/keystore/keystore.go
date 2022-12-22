793765e0dcff932e326c70245f87169e0df4e37e
Zealy-Rockx <jigao@rockx.com>
2022-12-22 15:57:38 +0800

    crypto/keystore: improve KeyStore #29

--- utils/crypto/keystore/keystore.go
+++ utils/crypto/keystore/keystore.go
@@ -3,21 +3,57 @@ package keystore
 import (
 	"github.com/ethereum/go-ethereum/accounts"
 	ethkeystore "github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
+	"os"
 )
 
 type KeyStore struct {
-	*ethkeystore.KeyStore
-	Account    *accounts.Account
-	Passphrase string
+	PasswordFile string
+	KeystoreDir  string
+	Address      common.Address
+
+	account  *accounts.Account
+	keystore *ethkeystore.KeyStore
+}
+
+func (ks *KeyStore) Init() error {
+	ethKeystore := ethkeystore.NewKeyStore(ks.KeystoreDir, ethkeystore.StandardScryptN, ethkeystore.StandardScryptP)
+	accounts_ := ethKeystore.Accounts()
+	for _, account := range accounts_ {
+		if account.Address == ks.Address {
+			ks.account = &account
+			break
+		}
+	}
+
+	if ks.account == nil {
+		return errors.New("found no account in keystore")
+	}
+
+	ks.keystore = ethKeystore
+	return nil
 }
 
 func (ks *KeyStore) Lock() error {
-	err := ks.KeyStore.Lock(ks.Account.Address)
+	err := ks.keystore.Lock(ks.account.Address)
 	return errors.WithStack(err)
 }
 
 func (ks *KeyStore) Unlock() error {
-	err := ks.KeyStore.Unlock(*ks.Account, ks.Passphrase)
+	// TODO: check 0400 for safe read
+	pass, err := os.ReadFile(ks.PasswordFile)
+	if err != nil {
+		return errors.Wrap(err, "failed to read file")
+	}
+	err = ks.keystore.Unlock(*ks.account, string(pass))
 	return errors.WithStack(err)
+}
+
+func (ks *KeyStore) Account() *accounts.Account {
+	return ks.account
+}
+
+func (ks *KeyStore) EthKeyStore() *ethkeystore.KeyStore {
+	return ks.keystore
 }
