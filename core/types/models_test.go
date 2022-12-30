f6a621b94d2c50c0082f99b39a01503ede9af207
Zealy-Rockx <jigao@rockx.com>
2022-12-30 17:24:03 +0800

    core/types: add tests for PubKey.#200

--- core/types/models_test.go
+++ core/types/models_test.go
@@ -1,8 +1,11 @@
 package types
 
 import (
+	"fmt"
 	"github.com/avalido/mpc-controller/utils/bytes"
+	"github.com/avalido/mpc-controller/utils/crypto"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
@@ -12,4 +15,40 @@ func TestRequestHash_SetTaskType(t *testing.T) {
 	reqHash.SetTaskType(TaskTypStake)
 	require.True(t, reqHash.IsTaskType(TaskTypStake))
 	require.Equal(t, TaskType(1), reqHash.TaskType())
+}
+
+// PubKey test suite
+type PubKeyTestSuite struct {
+	suite.Suite
+	pubKey PubKey
+}
+
+func (suite *PubKeyTestSuite) SetupTest() {
+	compressdGenPubKeyStr := "0241538c55805b2614cce2a9298d4479911409ded5e7ddf042629e76c38c237546" //
+	pubKeyBytes, err := crypto.DenormalizePubKeyFromHex(compressdGenPubKeyStr)
+	suite.Require().Nil(err)
+	suite.pubKey = pubKeyBytes
+}
+
+func (suite *PubKeyTestSuite) TestString() {
+	pubKeyStr := suite.pubKey.String()
+	fmt.Printf("Decomporessed key: %v\n", pubKeyStr)
+}
+
+func (suite *PubKeyTestSuite) TestCChainAddress() {
+	require := suite.Require()
+	cChainAddr, err := suite.pubKey.CChainAddress()
+	require.Nil(err)
+	fmt.Printf("C-Chain Address: %v\n", cChainAddr.String())
+}
+
+func (suite *PubKeyTestSuite) TestPChainAddress() {
+	require := suite.Require()
+	pChainAddr, err := suite.pubKey.PChainAddress()
+	require.Nil(err)
+	fmt.Printf("P-Chain Address: %v\n", pChainAddr.String())
+}
+
+func TestSignerTestSuite(t *testing.T) {
+	suite.Run(t, new(PubKeyTestSuite))
 }
