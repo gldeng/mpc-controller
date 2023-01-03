a9dd34e943e88fb63e60be19371723ea43d122eb
Zealy-Rockx <jigao@rockx.com>
2023-01-03 16:04:44 +0800

    subscriber: reorganize re-subscription. #139

--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -11,7 +11,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/event"
 	"github.com/pkg/errors"
-	"sync"
 	"time"
 )
 
@@ -23,9 +22,9 @@ type Subscriber struct {
 	subscription   ethereum.Subscription
 	eventLogQueue  Queue
 	eventIDGetter  EventIDGetter
-	clientRenewCh  chan struct{}
+	filter         ethereum.FilterQuery
+	clientRenewCh  chan *ethclient.Client
 	tryToReconnect bool
-	once           sync.Once
 }
 
 type Queue interface {
@@ -45,49 +44,24 @@ func NewSubscriber(ctx context.Context, logger logger.Logger, config core.Config
 		subscription:  nil,
 		eventLogQueue: eventLogQueue,
 		eventIDGetter: evtIDGetter,
-		clientRenewCh: make(chan struct{}),
+		filter:        ethereum.FilterQuery{Addresses: []common.Address{config.MpcManagerAddress}},
+		clientRenewCh: make(chan *ethclient.Client),
 	}, nil
 }
 
 func (s *Subscriber) Start() error {
-	if s.client == nil {
-		client, err := s.config.CreateWsClient()
-		if err != nil {
-			return errors.Wrap(err, "failed to create ws client")
-		}
-		s.client = client
-	}
-
-	filter := ethereum.FilterQuery{
-		Addresses: []common.Address{s.config.MpcManagerAddress},
-	}
-
-	eventLogs := make(chan types.Log, 1024)
-	sub, err := s.client.SubscribeFilterLogs(s.ctx, filter, eventLogs)
+	client, err := s.config.CreateWsClient()
 	if err != nil {
-		return errors.Wrap(err, "failed to subscribe to contract events")
+		return errors.Wrap(err, "failed to create ws client")
+	}
+	s.client = client
+
+	if err := s.subscribe(); err != nil {
+		return errors.Wrap(err, "failed to subscribe")
 	}
 
-	s.subscription = event.NewSubscription(func(quit <-chan struct{}) error {
-		defer sub.Unsubscribe()
-		for {
-			select {
-			case log := <-eventLogs:
-				s.updateMetrics(log)
-				s.eventLogQueue.Enqueue(log) // TODO: Handle failure. There should be only one publisher to this queue, so now it should be fine to ignore the error.
-			case err := <-sub.Err():
-				// TODO: Should we reconnect if this happens?
-				return err
-			case <-quit:
-				return nil
-			}
-		}
-	})
-
-	s.once.Do(func() {
-		go s.restartOnWsClientRecreated()
-		go s.recreateWsClientOnConnectionFailure()
-	})
+	go s.resubscribe()
+	go s.reconnect()
 
 	return nil
 }
@@ -104,45 +78,31 @@ func (s *Subscriber) Close() error {
 	return nil
 }
 
-func (s *Subscriber) updateMetrics(log types.Log) {
-	switch log.Topics[0] {
-	case s.eventIDGetter.GetEventID(core.EvtRequestStarted):
-	case s.eventIDGetter.GetEventID(core.EvtParticipantAdded):
-		prom.ContractEvtParticipantAdded.Inc()
-	case s.eventIDGetter.GetEventID(core.EvtKeygenRequestAdded):
-		prom.ContractEvtKeygenRequestAdded.Inc()
-	case s.eventIDGetter.GetEventID(core.EvtKeyGenerated):
-		prom.ContractEvtKeyGenerated.Inc()
-	case s.eventIDGetter.GetEventID(core.EvtStakeRequestAdded):
-		prom.ContractEvtStakeRequestAdded.Inc()
-	}
-}
-
-func (s *Subscriber) restartOnWsClientRecreated() {
+func (s *Subscriber) resubscribe() {
 	s.logger.Debug("waiting for ws client recreated")
 
 	for {
 		select {
 		case <-s.ctx.Done():
 			return
-		case <-s.clientRenewCh:
-		restart:
+		case c := <-s.clientRenewCh:
+		resubscribe:
 			for {
 				select {
 				case <-s.ctx.Done():
 					return
 				default:
-					if err := s.Close(); err != nil {
-						s.logger.Error("failed to close Subscriber", []logger.Field{{"error", err}}...)
-						break
-					}
-					if err := s.Start(); err != nil {
-						s.logger.Error("failed to restart Subscriber", []logger.Field{{"error", err}}...)
+					s.subscription.Unsubscribe()
+					s.client.Close()
+
+					s.client = c
+					if err := s.subscribe(); err != nil {
+						s.logger.Error("failed to re-subscribe", []logger.Field{{"error", err}}...)
 						break
 					}
 
-					s.logger.Debug("restarted Subscriber")
-					break restart
+					s.logger.Debug("re-subscribed")
+					break resubscribe
 
 				}
 				time.Sleep(time.Second)
@@ -151,7 +111,7 @@ func (s *Subscriber) restartOnWsClientRecreated() {
 	}
 }
 
-func (s *Subscriber) recreateWsClientOnConnectionFailure() {
+func (s *Subscriber) reconnect() {
 	s.logger.Debug("checking ws connectivity")
 
 	t := time.NewTicker(time.Second)
@@ -176,11 +136,51 @@ func (s *Subscriber) recreateWsClientOnConnectionFailure() {
 					s.logger.Error("failed to recreate ws client", []logger.Field{{"error", err}}...)
 					continue
 				}
-				s.client = client
-				s.clientRenewCh <- struct{}{}
+				s.clientRenewCh <- client
 				s.logger.Debug("ws client recreated")
 				s.tryToReconnect = false
 			}
 		}
+	}
+}
+
+func (s *Subscriber) subscribe() error {
+	eventLogs := make(chan types.Log, 1024)
+	sub, err := s.client.SubscribeFilterLogs(s.ctx, s.filter, eventLogs)
+	if err != nil {
+		return errors.Wrap(err, "failed to subscribe to contract events")
+	}
+
+	s.subscription = event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-eventLogs:
+				s.updateMetrics(log)
+				if err := s.eventLogQueue.Enqueue(log); err != nil {
+					s.logger.Error("failed to enqueue event log", []logger.Field{{"error", err}}...)
+				}
+			case err := <-sub.Err():
+				s.logger.Error("got an error for subscription", []logger.Field{{"error", err}}...)
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	})
+	return nil
+}
+
+func (s *Subscriber) updateMetrics(log types.Log) {
+	switch log.Topics[0] {
+	case s.eventIDGetter.GetEventID(core.EvtRequestStarted):
+	case s.eventIDGetter.GetEventID(core.EvtParticipantAdded):
+		prom.ContractEvtParticipantAdded.Inc()
+	case s.eventIDGetter.GetEventID(core.EvtKeygenRequestAdded):
+		prom.ContractEvtKeygenRequestAdded.Inc()
+	case s.eventIDGetter.GetEventID(core.EvtKeyGenerated):
+		prom.ContractEvtKeyGenerated.Inc()
+	case s.eventIDGetter.GetEventID(core.EvtStakeRequestAdded):
+		prom.ContractEvtStakeRequestAdded.Inc()
 	}
 }
