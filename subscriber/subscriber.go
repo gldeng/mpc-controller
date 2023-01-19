d320e676dcd87ca24dc624df615548c222f855e9
Zealy-Rockx <jigao@rockx.com>
2023-01-19 21:04:47 +0800

    subscriber, prom: Process historical events #181

--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -85,6 +85,13 @@ func (s *Subscriber) Start() error {
 			s.logger.Error("got an error for subscription", []logger.Field{{"error", err}}...)
 		}
 
+		err = s.compensateMissedLogs()
+		if err != nil {
+			s.logger.Error("failed to compensate missed logs", []logger.Field{{"error", err}}...)
+			return nil, err
+		}
+		prom.EventCompensation.With(prometheus.Labels{"type": "ethLog"}).Inc()
+
 		eventLogs := make(chan types.Log, eventLogChanCapacity)
 		sub, err := s.client.SubscribeFilterLogs(s.ctx, s.filter, eventLogs)
 		if err != nil {
@@ -101,56 +108,18 @@ func (s *Subscriber) Start() error {
 					return
 				case log := <-eventLogs:
 					s.updateMetricOnEvtID(log)
-					s.logger.Debug("received event log", []logger.Field{
-						{"address", log.Address},
-						{"txHash", log.TxHash},
-						{"topics", log.Topics},
-						{"blockNumber", log.BlockNumber},
-						{"index", log.Index},
-						{"removed", log.Removed}}...)
 
-					valid, err := s.verifyReceivedLog(log)
+					err := s.verifyStreamingLog(log)
 					if err != nil {
-						s.logger.Error("failed to verify received log", []logger.Field{
+						s.logger.Error("failed to verify streaming log", []logger.Field{
 							{"blockNumber", log.BlockNumber},
 							{"index", log.Index},
 							{"error", err}}...)
-						continue
-					}
-
-					if !valid {
 						continue
 					}
 
 					s.checkStakeReqNoContinuity(log)
-
-					s.queueBufferCh <- log
-					if err := s.eventLogQueue.Enqueue(<-s.queueBufferCh); err != nil {
-						prom.QueueOperationError.With(prometheus.Labels{"pkg": "subscriber", "operation": "enqueue"}).Inc()
-						s.logger.Error("failed to enqueue event log, enqueue error", []logger.Field{
-							{"blockNumber", log.BlockNumber},
-							{"index", log.Index},
-							{"error", err}}...)
-						continue
-					}
-					prom.QueueOperation.With(prometheus.Labels{"pkg": "subscriber", "operation": "enqueue"}).Inc()
-					s.logger.Debug("enqueued event log", []logger.Field{
-						{"blockNumber", log.BlockNumber},
-						{"index", log.Index}}...)
-
-					err = s.saveEnqueuedLog(enqueuedLog{log.BlockNumber, log.Index})
-					if err != nil {
-						s.logger.Error("failed to save enqueued event log", []logger.Field{
-							{"blockNumber", log.BlockNumber},
-							{"index", log.Index},
-							{"error", err}}...)
-						prom.DBOperationError.With(prometheus.Labels{"pkg": "subscriber", "operation": "save"}).Inc()
-						continue
-					}
-					prom.DBOperation.With(prometheus.Labels{"pkg": "subscriber", "operation": "save"}).Inc()
-					s.logger.Debug("saved enqueued event log", []logger.Field{
-						{"blockNumber", log.BlockNumber},
-						{"index", log.Index}}...)
+					_ = s.enqueueAndSaveLog(log)
 				case <-sub.Err():
 					return
 				}
@@ -181,42 +150,182 @@ type enqueuedLog struct {
 	Index       uint
 }
 
-// validateReceivedLog verifies the block number and index of a received eth log against those of stored enqueued log
-func (s *Subscriber) verifyReceivedLog(receivedLog types.Log) (bool, error) {
+// compensateMissedLogs compensate missed eth log because of exceptions, such as network partition
+func (s *Subscriber) compensateMissedLogs() error {
+	found, err := s.foundEnqueuedLog()
+	if err != nil {
+		prom.EventCompensationError.With(prometheus.Labels{"type": "ethLog", "reason": "readDb"}).Inc()
+		return errors.Wrap(err, "failed to check the existence of enqueued log")
+	}
+
+	if !found {
+		return nil
+	}
+
+	storedLog, err := s.loadEnqueuedLog()
+	if err != nil {
+		prom.EventCompensationError.With(prometheus.Labels{"type": "ethLog", "reason": "readDb"}).Inc()
+		return errors.Wrap(err, "failed to load enqueued log")
+	}
+
+	maybeMissedLogs, err := s.filterLatestLogs(storedLog.BlockNumber)
+	if err != nil {
+		prom.EventCompensationError.With(prometheus.Labels{"type": "ethLog", "reason": "filterLog"}).Inc()
+		return errors.Wrap(err, "failed to filter latest logs")
+	}
+
+	if len(maybeMissedLogs) == 0 {
+		return nil
+	}
+
+	for _, maybeMissedLog := range maybeMissedLogs {
+		s.logger.Debug("maybe missed log", []logger.Field{
+			{"blockNumber", maybeMissedLog.BlockNumber},
+			{"index", maybeMissedLog.Index},
+			{}}...)
+	}
+
+	var missedFrom int
+
+	for i, maybeMissedLog := range maybeMissedLogs {
+		if maybeMissedLog.BlockNumber < storedLog.BlockNumber {
+			continue
+		}
+		if maybeMissedLog.BlockNumber == storedLog.BlockNumber {
+			if maybeMissedLog.Index <= storedLog.Index {
+				continue
+			}
+			missedFrom = i
+			break
+		}
+		missedFrom = i
+		break
+	}
+
+	missedLogs := maybeMissedLogs[missedFrom:]
+	if len(missedLogs) == 0 {
+		s.logger.Debug("no missed log found")
+		return nil
+	}
+
+	for _, missedLog := range maybeMissedLogs {
+		s.logger.Debug("found missed log", []logger.Field{
+			{"blockNumber", missedLog.BlockNumber},
+			{"index", missedLog.Index}}...)
+	}
+
+	err = s.enqueueAndSaveLogs(missedLogs)
+	if err != nil {
+		prom.EventCompensationError.With(prometheus.Labels{"type": "ethLog", "reason": "enqueueAndSaveLog"}).Inc()
+		return errors.Wrap(err, "failed to enqueue and save logs")
+	}
+	return nil
+}
+
+func (s *Subscriber) filterLatestLogs(fromBlock uint64) ([]types.Log, error) {
+	filterQuery := ethereum.FilterQuery{
+		Addresses: []common.Address{s.config.MpcManagerAddress},
+		FromBlock: big.NewInt(int64(fromBlock)),
+		ToBlock:   nil, // latest block
+		Topics:    nil, // any topic list
+	}
+	logs, err := s.client.FilterLogs(s.ctx, filterQuery)
+	return logs, errors.Wrap(err, "failed to filter logs")
+}
+
+func (s *Subscriber) enqueueAndSaveLogs(logs []types.Log) error {
+	for _, log := range logs {
+		s.updateMetricOnEvtID(log)
+		s.checkStakeReqNoContinuity(log)
+
+		err := s.enqueueAndSaveLog(log)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+func (s *Subscriber) enqueueAndSaveLog(log types.Log) error {
+	var gotErr error
+	defer func() {
+		if gotErr != nil {
+			s.logger.Error("failed to enqueue and save event log", []logger.Field{
+				{"blockNumber", log.BlockNumber},
+				{"index", log.Index},
+				{"error", gotErr}}...)
+			return
+		}
+		s.logger.Debug("enqueued and saved event log", []logger.Field{
+			{"blockNumber", log.BlockNumber},
+			{"index", log.Index}}...)
+	}()
+
+	s.queueBufferCh <- log
+	if err := s.eventLogQueue.Enqueue(<-s.queueBufferCh); err != nil {
+		prom.QueueOperationError.With(prometheus.Labels{"pkg": "subscriber", "operation": "enqueue"}).Inc()
+		gotErr = err
+		return errors.WithStack(err)
+	}
+	prom.QueueOperation.With(prometheus.Labels{"pkg": "subscriber", "operation": "enqueue"}).Inc()
+
+	err := s.saveEnqueuedLog(enqueuedLog{log.BlockNumber, log.Index})
+	if err != nil {
+		prom.DBOperationError.With(prometheus.Labels{"pkg": "subscriber", "operation": "save"}).Inc()
+		gotErr = err
+		return errors.WithStack(err)
+	}
+	prom.DBOperation.With(prometheus.Labels{"pkg": "subscriber", "operation": "save"}).Inc()
+	return nil
+}
+
+// verifyStreamingLog verifies the block number and index of a streaming eth log against those of stored enqueued log
+func (s *Subscriber) verifyStreamingLog(streamingLog types.Log) error {
+	found, err := s.foundEnqueuedLog()
+	if err != nil {
+		return errors.Wrap(err, "failed to check enqueued log existence")
+	}
+
+	if !found {
+		return nil
+	}
+
+	storedLog, err := s.loadEnqueuedLog()
+	if err != nil {
+		return errors.Wrap(err, "failed to load enqueued log")
+	}
+
+	if streamingLog.BlockNumber < storedLog.BlockNumber {
+		prom.InvalidStreamingEvent.With(prometheus.Labels{"type": "ethLog", "field": "blockNumber"}).Inc()
+		return errors.Errorf("invalid block number, minimumAccepted:%v, got:%v", storedLog.BlockNumber, streamingLog.BlockNumber)
+	}
+
+	if streamingLog.BlockNumber == storedLog.BlockNumber && streamingLog.Index <= storedLog.Index {
+		prom.InvalidStreamingEvent.With(prometheus.Labels{"type": "ethLog", "field": "index"}).Inc()
+		return errors.Errorf("invalid index, minimumAccepted:%v, got:%v", storedLog.Index+1, streamingLog.Index)
+	}
+
+	return nil
+}
+
+func (s *Subscriber) foundEnqueuedLog() (bool, error) {
 	found, err := s.db.Exists(s.ctx, enqueuedLogKey)
 	if err != nil {
 		return false, errors.WithStack(err)
 	}
 
-	if !found {
-		return true, nil
-	}
+	return found, nil
+}
 
+func (s *Subscriber) loadEnqueuedLog() (enqueuedLog, error) {
 	storedBytes, err := s.db.Get(s.ctx, enqueuedLogKey)
 	if err != nil {
-		return false, errors.WithStack(err)
+		return enqueuedLog{}, errors.WithStack(err)
 	}
 
 	var storedLog enqueuedLog
 	_ = json.Unmarshal(storedBytes, &storedLog)
-
-	if receivedLog.BlockNumber < storedLog.BlockNumber {
-		prom.InvalidReceivedEvent.With(prometheus.Labels{"type": "ethLog", "field": "blockNumber"}).Inc()
-		s.logger.Error("invalid block number", []logger.Field{
-			{"minimumAccepted", storedLog.BlockNumber},
-			{"got", receivedLog.BlockNumber}}...)
-		return false, nil
-	}
-
-	if receivedLog.BlockNumber == storedLog.BlockNumber && receivedLog.Index <= storedLog.Index {
-		prom.InvalidReceivedEvent.With(prometheus.Labels{"type": "ethLog", "field": "index"}).Inc()
-		s.logger.Error("invalid block number", []logger.Field{
-			{"minimumAccepted", storedLog.Index + 1},
-			{"got", receivedLog.Index}}...)
-		return false, nil
-	}
-
-	return true, nil
+	return storedLog, nil
 }
 
 func (s *Subscriber) saveEnqueuedLog(log enqueuedLog) error {
@@ -226,6 +335,14 @@ func (s *Subscriber) saveEnqueuedLog(log enqueuedLog) error {
 }
 
 func (s *Subscriber) updateMetricOnEvtID(log types.Log) {
+	s.logger.Debug("received event log", []logger.Field{
+		{"address", log.Address},
+		{"txHash", log.TxHash},
+		{"topics", log.Topics},
+		{"blockNumber", log.BlockNumber},
+		{"index", log.Index},
+		{"removed", log.Removed}}...)
+
 	switch log.Topics[0] {
 	case s.eventIDGetter.GetEventID(core.EvtRequestStarted):
 	case s.eventIDGetter.GetEventID(core.EvtParticipantAdded):
@@ -246,6 +363,7 @@ func (s *Subscriber) checkStakeReqNoContinuity(log types.Log) {
 		err := s.logUnpacker.UnpackLog(event, core.EvtStakeRequestAdded, log)
 		if err != nil {
 			s.logger.Error("failed to unpack log for EvtStakeRequestAdded", []logger.Field{{"error", err}}...)
+			return
 		}
 
 		newStakeReqNo := event.RequestNumber
