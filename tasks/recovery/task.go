133bd8e89eaedfd62abf42acbbfbd0569ff57376
gldeng <realgldeng@gmail.com>
2023-02-01 10:15:55 +0800

    Draft #182

--- tasks/recovery/task.go
+++ tasks/recovery/task.go
@@ -26,10 +26,10 @@ const (
 )
 
 var (
-	_ core.Task = (*ExportFromCChain)(nil)
+	_ core.Task = (*Recovery)(nil)
 )
 
-type ExportFromCChain struct {
+type Recovery struct {
 	Status      Status
 	FlowId      string
 	TaskType    string
@@ -44,24 +44,24 @@ type ExportFromCChain struct {
 	StartTime   *time.Time
 }
 
-func (t *ExportFromCChain) GetId() string {
+func (t *Recovery) GetId() string {
 	return fmt.Sprintf("%v-export", t.FlowId)
 }
 
-func (t *ExportFromCChain) FailedPermanently() bool {
+func (t *Recovery) FailedPermanently() bool {
 	return t.Failed
 }
 
-func (t *ExportFromCChain) IsSequential() bool {
+func (t *Recovery) IsSequential() bool {
 	return true
 }
 
-func (t *ExportFromCChain) IsDone() bool {
+func (t *Recovery) IsDone() bool {
 	return t.Status == StatusDone
 }
 
-func NewExportFromCChain(flowId string, quorum types.QuorumInfo, amount big.Int) (*ExportFromCChain, error) {
-	return &ExportFromCChain{
+func NewRecovery(flowId string, quorum types.QuorumInfo, amount big.Int) (*Recovery, error) {
+	return &Recovery{
 		Status:      StatusInit,
 		FlowId:      flowId,
 		TaskType:    taskTypeExport,
@@ -76,7 +76,7 @@ func NewExportFromCChain(flowId string, quorum types.QuorumInfo, amount big.Int)
 	}, nil
 }
 
-func (t *ExportFromCChain) Next(ctx core.TaskContext) ([]core.Task, error) {
+func (t *Recovery) Next(ctx core.TaskContext) ([]core.Task, error) {
 	if t.StartTime == nil {
 		now := time.Now()
 		t.StartTime = &now
@@ -105,11 +105,11 @@ func (t *ExportFromCChain) Next(ctx core.TaskContext) ([]core.Task, error) {
 	}
 }
 
-func (t *ExportFromCChain) SignedTx() (*evm.Tx, error) {
+func (t *Recovery) SignedTx() (*evm.Tx, error) {
 	return PackSignedExportTx(t.Tx, t.TxCred)
 }
 
-func (t *ExportFromCChain) run(ctx core.TaskContext) ([]core.Task, error) {
+func (t *Recovery) run(ctx core.TaskContext) ([]core.Task, error) {
 	switch t.Status {
 	case StatusInit:
 		err := t.buildAndSignTx(ctx)
@@ -137,7 +137,7 @@ func (t *ExportFromCChain) run(ctx core.TaskContext) ([]core.Task, error) {
 	return nil, nil
 }
 
-func (t *ExportFromCChain) buildSignReq(id string, hash []byte) (*mpc.SignRequest, error) {
+func (t *Recovery) buildSignReq(id string, hash []byte) (*mpc.SignRequest, error) {
 	partiPubKeys, genPubKey, err := t.Quorum.CompressKeys()
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to compress public keys")
@@ -150,7 +150,7 @@ func (t *ExportFromCChain) buildSignReq(id string, hash []byte) (*mpc.SignReques
 	return s, nil
 }
 
-func (t *ExportFromCChain) buildAndSignTx(ctx core.TaskContext) error {
+func (t *Recovery) buildAndSignTx(ctx core.TaskContext) error {
 	builder := NewTxBuilder(ctx.GetNetwork())
 	nonce, err := ctx.NonceAt(t.Quorum.CChainAddress())
 	if err != nil {
@@ -162,7 +162,7 @@ func (t *ExportFromCChain) buildAndSignTx(ctx core.TaskContext) error {
 	if err != nil {
 		return t.failIfErrorf(err, ErrMsgFailedToConvertAmount)
 	}
-	tx, err := builder.ExportFromCChain(t.Quorum.PubKey, amount, nonce)
+	tx, err := builder.Recovery(t.Quorum.PubKey, amount, nonce)
 	if err != nil {
 		return t.failIfErrorf(err, ErrMsgFailedToBuildTx)
 	}
@@ -197,7 +197,7 @@ func (t *ExportFromCChain) buildAndSignTx(ctx core.TaskContext) error {
 	return nil
 }
 
-func (t *ExportFromCChain) getSignature(ctx core.TaskContext) error {
+func (t *Recovery) getSignature(ctx core.TaskContext) error {
 	res, err := ctx.GetMpcClient().CheckResult(context.Background(), &mpc.CheckResultRequest{RequestId: t.SignRequest.RequestId})
 	// TODO: Handle 404
 	if err != nil {
@@ -237,7 +237,7 @@ func (t *ExportFromCChain) getSignature(ctx core.TaskContext) error {
 //     avalanche network already before sending tx
 //  3. check tx status again after sending failed which may be caused by another participant sending the same tx
 //     at the same time
-func (t *ExportFromCChain) sendTx(ctx core.TaskContext) error {
+func (t *Recovery) sendTx(ctx core.TaskContext) error {
 	// waits for arbitrary duration to elapse to reduce race condition.
 	utilstime.RandomDelay(5000)
 
@@ -263,7 +263,7 @@ func (t *ExportFromCChain) sendTx(ctx core.TaskContext) error {
 	return nil
 }
 
-func (t *ExportFromCChain) checkIfTxIssued(ctx core.TaskContext) (bool, error) {
+func (t *Recovery) checkIfTxIssued(ctx core.TaskContext) (bool, error) {
 	status, err := t.checkTxStatus(ctx)
 	if err != nil {
 		t.logError(ctx, ErrMsgCheckTxStatusFail, err, []logger.Field{{"txId", t.TxID}}...)
@@ -291,7 +291,7 @@ func (t *ExportFromCChain) checkIfTxIssued(ctx core.TaskContext) (bool, error) {
 	}
 }
 
-func (t *ExportFromCChain) checkTxStatus(ctx core.TaskContext) (core.TxStatus, error) {
+func (t *Recovery) checkTxStatus(ctx core.TaskContext) (core.TxStatus, error) {
 	status, err := ctx.CheckCChainTx(*t.TxID)
 	if err != nil {
 		return status, errors.WithStack(err)
@@ -316,7 +316,7 @@ func (t *ExportFromCChain) checkTxStatus(ctx core.TaskContext) (core.TxStatus, e
 	}
 }
 
-func (t *ExportFromCChain) failIfErrorf(err error, format string, a ...any) error {
+func (t *Recovery) failIfErrorf(err error, format string, a ...any) error {
 	if err == nil {
 		return nil
 	}
@@ -324,7 +324,7 @@ func (t *ExportFromCChain) failIfErrorf(err error, format string, a ...any) erro
 	return errors.Wrap(err, fmt.Sprintf(format, a...))
 }
 
-func (t *ExportFromCChain) logDebug(ctx core.TaskContext, msg string, fields ...logger.Field) {
+func (t *Recovery) logDebug(ctx core.TaskContext, msg string, fields ...logger.Field) {
 	allFields := make([]logger.Field, 0, len(fields)+2)
 	allFields = append(allFields, logger.Field{"flowId", t.FlowId})
 	allFields = append(allFields, logger.Field{"taskType", t.TaskType})
@@ -332,7 +332,7 @@ func (t *ExportFromCChain) logDebug(ctx core.TaskContext, msg string, fields ...
 	ctx.GetLogger().Debug(msg, allFields...)
 }
 
-func (t *ExportFromCChain) logInfo(ctx core.TaskContext, msg string, fields ...logger.Field) {
+func (t *Recovery) logInfo(ctx core.TaskContext, msg string, fields ...logger.Field) {
 	allFields := make([]logger.Field, 0, len(fields)+2)
 	allFields = append(allFields, logger.Field{"flowId", t.FlowId})
 	allFields = append(allFields, logger.Field{"taskType", t.TaskType})
@@ -340,7 +340,7 @@ func (t *ExportFromCChain) logInfo(ctx core.TaskContext, msg string, fields ...l
 	ctx.GetLogger().Info(msg, allFields...)
 }
 
-func (t *ExportFromCChain) logError(ctx core.TaskContext, msg string, err error, fields ...logger.Field) {
+func (t *Recovery) logError(ctx core.TaskContext, msg string, err error, fields ...logger.Field) {
 	allFields := make([]logger.Field, 0, len(fields)+3)
 	allFields = append(allFields, logger.Field{"flowId", t.FlowId})
 	allFields = append(allFields, logger.Field{"taskType", t.TaskType})
